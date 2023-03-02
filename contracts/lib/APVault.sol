2b8cd7b7a8191791998e7654e91437524295fa99
katzman <steve.r.katzman@gmail.com>
2023-03-02 00:36:34 +0000

    Stashing changes before switching branches

--- contracts/lib/APVault.sol
+++ contracts/lib/APVault.sol
@@ -3,16 +3,19 @@
 pragma solidity >=0.8.0;
 
 import {IVault} from "../interfaces/IVault.sol";
+import {IRegistrar} from "../interfaces/IRegistrar.sol";
 import {ERC4626} from "./ERC4626.sol";
 import {ERC20} from "./ERC20.sol";
 
 
 abstract contract APVault is IVault, ERC4626 {
 
-  constructor(address _regsitrar, ERC20 _asset, string memory _name, string memory _symbol) 
+  IRegistrar immutable registrar;
+
+  constructor(IRegistrar _registrar, ERC20 _asset, string memory _name, string memory _symbol) 
     ERC4626(_asset, _name, _symbol)
   {
-
+    registrar = _registrar;
   }
 
   function deposit(uint256 assets, address receiver) public virtual override returns (uint256 shares) {
@@ -39,5 +42,24 @@ abstract contract APVault is IVault, ERC4626 {
     
   }
 
+  modifier approvedRouterOnly() {
+    require(_isApprovedRouter(), "Not approved Router");
+    _;
+  }
+  
+  function _isApprovedRouter() internal view override returns (bool){
+    IRegistrar.AngelProtocolParams memory apParams = registrar.getAngelProtocolParams();
+    return(apParams.routerAddr == msg.sender);
+  }
 
+  // Either the router or the sibling vault
+  modifier approvedOnly(bytes4 STRATEGY_ID) {
+    require(_isApprovedRouter() || _isSiblingVault(STRATEGY_ID), "Not approved");
+    _;
+  }
+
+  function _isSiblingVault(bytes4 STRATEGY_ID) internal view returns (bool) {
+    IRegistrar.StrategyParams memory stratParams = registrar.getStrategyParamsById(STRATEGY_ID);
+    return(msg.sender == stratParams.Locked.vaultAddr);
+  }
 }
