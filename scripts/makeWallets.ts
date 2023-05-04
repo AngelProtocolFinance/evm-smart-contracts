360513eb4f70a5eb59f067963b6ef11d2befbdc0
katzman <steve.r.katzman@gmail.com>
2023-05-04 17:38:21 +0000

    Added script for generating new wallets

---
+++ scripts/makeWallets.ts
@@ -0,0 +1,12 @@
+import { genWallet } from "../utils/keygen";
+
+async function main() {
+  let a = genWallet(true)
+} 
+
+main()
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.error(error);
+    process.exit(1);
+  });
