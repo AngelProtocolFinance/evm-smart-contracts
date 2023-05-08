bfe3102239721259e108c2984b70f616d97e18bd
Nenad <n.misic@outlook.com>
2023-05-08 08:02:03 +0200

    Revert change to changeAdmin format

--- tasks/manage/changeAdmin.ts
+++ tasks/manage/changeAdmin.ts
@@ -1,13 +1,10 @@
-import { SignerWithAddress } from "@nomiclabs/hardhat-ethers/signers";
-import { task } from "hardhat/config";
-import { HardhatRuntimeEnvironment } from "hardhat/types";
-import { createInterface } from "node:readline/promises";
-import addresses from "../../contract-address.json";
-import {
-    ITransparentUpgradeableProxy__factory,
-    OwnershipFacet__factory,
-} from "../../typechain-types";
-import * as logger from "../../utils/logger";
+import { SignerWithAddress } from "@nomiclabs/hardhat-ethers/signers"
+import { task } from "hardhat/config"
+import { HardhatRuntimeEnvironment } from "hardhat/types"
+import { createInterface } from "node:readline/promises"
+import addresses from "../../contract-address.json"
+import { ITransparentUpgradeableProxy__factory, OwnershipFacet__factory } from "../../typechain-types"
+import * as logger from "../../utils/logger"
 
 task("manage:changeAdmin", "Will update the admin for all proxy contracts")
     .addParam("currentAdmin", "Current admin address")
@@ -17,106 +14,70 @@ task("manage:changeAdmin", "Will update the admin for all proxy contracts")
             const readline = createInterface({
                 input: process.stdin,
                 output: process.stdout,
-            });
+            })
 
-            const answer = await readline.question(
-                `Are you sure you wish to set ${taskArguments.newAdmin} as the new admin? (Y/y)`
-            );
+            const answer = await readline.question(`Are you sure you wish to set ${taskArguments.newAdmin} as the new admin? (Y/y)`)
 
             if (!/^(|y|yes)$/i.test(answer)) {
-                return console.log("Aborting...");
+                return console.log("Aborting...")
             }
 
-            const currentAdmin = await hre.ethers.getSigner(
-                taskArguments.currentAdmin
-            );
+            const currentAdmin = await hre.ethers.getSigner(taskArguments.currentAdmin)
 
-            await transferAccountOwnership(
-                currentAdmin,
-                taskArguments.newAdmin,
-                hre
-            );
+            await transferAccountOwnership(currentAdmin, taskArguments.newAdmin, hre)
 
-            await changeProxiesAdmin(currentAdmin, taskArguments.newAdmin, hre);
+            await changeProxiesAdmin(currentAdmin, taskArguments.newAdmin, hre)
         } catch (error) {
-            logger.out(error, logger.Level.Error);
+            logger.out(error, logger.Level.Error)
         } finally {
-            console.log("Done.");
+            console.log("Done.")
         }
-    });
+    })
 
-async function transferAccountOwnership(
-    currentAdmin: SignerWithAddress,
-    newAdmin: string,
-    hre: HardhatRuntimeEnvironment
-) {
+async function transferAccountOwnership(currentAdmin: SignerWithAddress, newAdmin: string, hre: HardhatRuntimeEnvironment) {
     try {
-        const ownershipFacet = OwnershipFacet__factory.connect(
-            addresses.accounts.diamond,
-            currentAdmin
-        );
-        const tx = await ownershipFacet.transferOwnership(newAdmin);
-        await hre.ethers.provider.waitForTransaction(tx.hash);
-        console.log("Transferred Account diamond ownership.");
+        const ownershipFacet = OwnershipFacet__factory.connect(addresses.accounts.diamond, currentAdmin)
+        const tx = await ownershipFacet.transferOwnership(newAdmin)
+        await hre.ethers.provider.waitForTransaction(tx.hash)
+        console.log("Transferred Account diamond ownership.")
     } catch (error) {
-        logger.out(
-            `Failed to change admin for Account diamond, reason: ${error}`,
-            logger.Level.Error
-        );
+        logger.out(`Failed to change admin for Account diamond, reason: ${error}`, logger.Level.Error)
     }
 }
 
-async function changeProxiesAdmin(
-    currentAdmin: SignerWithAddress,
-    taskArguments: any,
-    hre: HardhatRuntimeEnvironment
-) {
-    console.log("Reading proxy contract addresses...");
+async function changeProxiesAdmin(currentAdmin: SignerWithAddress, taskArguments: any, hre: HardhatRuntimeEnvironment) {
+    console.log("Reading proxy contract addresses...")
 
-    const proxies = extractProxyContractAddresses("", addresses);
+    const proxies = extractProxyContractAddresses("", addresses)
 
     for (const proxy of proxies) {
         try {
-            const upgradeableProxy =
-                ITransparentUpgradeableProxy__factory.connect(
-                    proxy.address,
-                    currentAdmin
-                );
-            const tx = await upgradeableProxy.changeAdmin(
-                taskArguments.newAdmin
-            );
-            await hre.ethers.provider.waitForTransaction(tx.hash);
-            console.log(`Changed admin for ${proxy.name}.`);
+            const upgradeableProxy = ITransparentUpgradeableProxy__factory.connect(proxy.address, currentAdmin)
+            const tx = await upgradeableProxy.changeAdmin(taskArguments.newAdmin)
+            await hre.ethers.provider.waitForTransaction(tx.hash)
+            console.log(`Changed admin for ${proxy.name}.`)
         } catch (error) {
-            logger.out(
-                `Failed to change admin for ${proxy.name}, reason: ${error}`,
-                logger.Level.Error
-            );
+            logger.out(`Failed to change admin for ${proxy.name}, reason: ${error}`, logger.Level.Error)
         }
     }
 }
 
-function extractProxyContractAddresses(
-    key: string,
-    value: any
-): { name: string; address: string }[] {
+function extractProxyContractAddresses(key: string, value: any): { name: string; address: string }[] {
     if (!value) {
-        return [];
+        return []
     }
 
     if (typeof value === "string") {
         if (key.includes("Proxy")) {
-            return [{ name: key, address: value }];
+            return [{ name: key, address: value }]
         } else {
-            return [];
+            return []
         }
     }
 
     if (typeof value !== "object") {
-        return [];
+        return []
     }
 
-    return Object.entries(value).flatMap(([key, val]) =>
-        extractProxyContractAddresses(key, val)
-    );
+    return Object.entries(value).flatMap(([key, val]) => extractProxyContractAddresses(key, val))
 }
