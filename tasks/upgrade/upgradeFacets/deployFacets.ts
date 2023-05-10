a6665f24ec58701321796ed86dee10d5773fee6f
Nenad <n.misic@outlook.com>
2023-05-10 09:41:06 +0200

    Refactor

---
+++ tasks/upgrade/upgradeFacets/deployFacets.ts
@@ -0,0 +1,168 @@
+import { SignerWithAddress } from "@nomiclabs/hardhat-ethers/signers"
+import { ContractFactory } from "ethers"
+import { FacetCutAction, getSelectors } from "../../../contracts/core/accounts/scripts/libraries/diamond"
+import {
+    AccountDepositWithdrawEndowments__factory,
+    AccountDonationMatch__factory,
+    AccountsAllowance__factory,
+    AccountsCreateEndowment__factory,
+    AccountsDAOEndowments__factory,
+    AccountsQueryEndowments__factory,
+    AccountsStrategiesCopyEndowments__factory,
+    AccountsStrategiesUpdateEndowments__factory,
+    AccountsSwapEndowments__factory,
+    AccountsUpdateEndowmentSettingsController__factory,
+    AccountsUpdateEndowments__factory,
+    AccountsUpdateStatusEndowments__factory,
+    AccountsVaultFacet__factory,
+    AxelarExecutionContract__factory,
+} from "../../../typechain-types"
+import * as logger from "../../../utils/logger"
+import { FacetCut } from "./types"
+
+export default async function deployFacets(
+    facetNames: string[],
+    diamondOwner: SignerWithAddress,
+    corestruct: string,
+    stringlib: string
+): Promise<FacetCut[]> {
+    const cuts: FacetCut[] = []
+
+    console.log("Instantiating factories...")
+
+    const facetFactories = await getFacetFactories(facetNames, diamondOwner, corestruct, stringlib)
+
+    console.log("Deploying all facets that use AccountStorage struct...")
+
+    for (const facetFactory of facetFactories) {
+        const { facetName, factory } = facetFactory
+        try {
+            const facet = await factory.deploy()
+            await facet.deployed()
+            console.log(`${facetName} deployed: ${facet.address}`)
+            cuts.push({
+                facetName,
+                cut: {
+                    facetAddress: facet.address,
+                    action: FacetCutAction.Replace,
+                    functionSelectors: getSelectors(facet),
+                },
+            })
+        } catch (error) {
+            logger.out(`Failed to deploy ${facetName}, reason: ${error}`, logger.Level.Error)
+        }
+    }
+    return cuts
+}
+
+type FacetFactory = { facetName: string; factory: ContractFactory }
+
+// Getting factories instantiated in bulk as they share the deploy/cut creation logic.
+async function getFacetFactories(
+    facetNames: string[],
+    diamondOwner: SignerWithAddress,
+    corestruct: string,
+    stringlib: string
+): Promise<FacetFactory[]> {
+    const factories: FacetFactory[] = []
+    const nonExistentFacets: string[] = []
+
+    facetNames.forEach((facetName) => {
+        const factory = getFacetFactory(facetName, diamondOwner, corestruct, stringlib)
+        if (typeof factory === "string") {
+            nonExistentFacets.push(factory)
+        } else {
+            factories.push({ facetName, factory })
+        }
+    })
+
+    if (!nonExistentFacets.length) {
+        return factories
+    }
+
+    throw new Error(`Nonexistent facets detected: ${nonExistentFacets.join(", ")}.`)
+}
+
+function getFacetFactory(
+    facetName: string,
+    diamondOwner: SignerWithAddress,
+    corestruct: string,
+    stringlib: string
+): ContractFactory | string {
+    switch (facetName) {
+        // no lib
+        case getFacetName(AccountsAllowance__factory):
+            return new AccountsAllowance__factory(diamondOwner)
+        case getFacetName(AccountsDAOEndowments__factory):
+            return new AccountsDAOEndowments__factory(diamondOwner)
+        case getFacetName(AccountDonationMatch__factory):
+            return new AccountDonationMatch__factory(diamondOwner)
+        case getFacetName(AccountsStrategiesCopyEndowments__factory):
+            return new AccountsStrategiesCopyEndowments__factory(diamondOwner)
+        // core lib
+        case getFacetName(AccountsCreateEndowment__factory):
+            return new AccountsCreateEndowment__factory(
+                { "contracts/core/struct.sol:AngelCoreStruct": corestruct },
+                diamondOwner
+            )
+        case getFacetName(AccountsQueryEndowments__factory):
+            return new AccountsQueryEndowments__factory(
+                { "contracts/core/struct.sol:AngelCoreStruct": corestruct },
+                diamondOwner
+            )
+        case getFacetName(AccountsSwapEndowments__factory):
+            return new AccountsSwapEndowments__factory(
+                { "contracts/core/struct.sol:AngelCoreStruct": corestruct },
+                diamondOwner
+            )
+        case getFacetName(AccountsUpdateEndowments__factory):
+            return new AccountsUpdateEndowments__factory(
+                { "contracts/core/struct.sol:AngelCoreStruct": corestruct },
+                diamondOwner
+            )
+        case getFacetName(AccountsUpdateEndowmentSettingsController__factory):
+            return new AccountsUpdateEndowmentSettingsController__factory(
+                { "contracts/core/struct.sol:AngelCoreStruct": corestruct },
+                diamondOwner
+            )
+        case getFacetName(AxelarExecutionContract__factory):
+            return new AxelarExecutionContract__factory(
+                { "contracts/core/struct.sol:AngelCoreStruct": corestruct },
+                diamondOwner
+            )
+        // string lib
+        case getFacetName(AccountsStrategiesUpdateEndowments__factory):
+            return new AccountsStrategiesUpdateEndowments__factory(
+                { "contracts/lib/Strings/string.sol:StringArray": stringlib },
+                diamondOwner
+            )
+        case getFacetName(AccountsUpdateStatusEndowments__factory):
+            return new AccountsUpdateStatusEndowments__factory(
+                { "contracts/lib/Strings/string.sol:StringArray": stringlib },
+                diamondOwner
+            )
+        // all libs
+        case getFacetName(AccountDepositWithdrawEndowments__factory):
+            return new AccountDepositWithdrawEndowments__factory(
+                {
+                    "contracts/core/struct.sol:AngelCoreStruct": corestruct,
+                    "contracts/lib/Strings/string.sol:StringArray": stringlib,
+                },
+                diamondOwner
+            )
+        case getFacetName(AccountsVaultFacet__factory):
+            return new AccountsVaultFacet__factory(
+                {
+                    "contracts/core/struct.sol:AngelCoreStruct": corestruct,
+                    "contracts/lib/Strings/string.sol:StringArray": stringlib,
+                },
+                diamondOwner
+            )
+        default:
+            return facetName
+    }
+}
+
+function getFacetName<T extends new (...args: any[]) => any>(clazz: T): string {
+    return clazz.name.replace("__factory", "")
+}
