a8afbf48edefaae3d2381b545af0c721d6e1c811
Nenad <n.misic@outlook.com>
2023-05-12 07:57:36 +0200

    Update upgradeMultisigs task

--- tasks/upgrade/upgradeMultisigs.ts
+++ tasks/upgrade/upgradeMultisigs.ts
@@ -19,11 +19,11 @@ task("upgrade:upgradeMultisig", "Will upgrade the implementation of the AP Team 
             let IMPLEMENTATION_ADDRESS_SLOT = "0x360894a13ba1a3210667c828492db98dca3e2076cc3735a920a3ca505d382bbc"
 
             // Get the new Multisig Factories
-            const APMultisig = new APTeamMultiSig__factory()
+            const APMultisig = new APTeamMultiSig__factory(proxyAdmin)
             const apMultisigImpl = await APMultisig.deploy()
             await apMultisigImpl.deployed()
 
-            const AppsMultisig = new ApplicationsMultiSig__factory()
+            const AppsMultisig = new ApplicationsMultiSig__factory(proxyAdmin)
             const appsMultisigImpl = await AppsMultisig.deploy()
             await appsMultisigImpl.deployed()
 
@@ -48,12 +48,17 @@ task("upgrade:upgradeMultisig", "Will upgrade the implementation of the AP Team 
             )
             console.log("Current Apps impl: ", currentAppsImpl)
             console.log("For proxy at: ", ApplicationsProxy.address)
-
-            // Send the upgrade call and wait for the tx to be finalized
+            
+            // Send the upgrade call and wait for the tx to be finalized 
+            logger.out("Upgrading APTeamMultiSig proxy implementation...")
             let tx1 = await APTeamProxy.upgradeTo(apMultisigImpl.address)
-            await hre.ethers.provider.waitForTransaction(tx1.hash)
+            logger.out(`Tx hash: ${tx1.hash}`)
+            await hre.ethers.provider.waitForTransaction(tx1.hash) 
+
+            logger.out("Upgrading ApplicationsMultiSig proxy implementation...")
             let tx2 = await ApplicationsProxy.upgradeTo(appsMultisigImpl.address)
             await hre.ethers.provider.waitForTransaction(tx2.hash)
+            logger.out(`Tx hash: ${tx1.hash}`)
 
             // Confirm that the proxy is pointed to the new implementation
             let newAPImpl = await hre.ethers.provider.getStorageAt(APTeamProxy.address, IMPLEMENTATION_ADDRESS_SLOT)
@@ -75,15 +80,20 @@ task("upgrade:upgradeMultisig", "Will upgrade the implementation of the AP Team 
             await saveFrontendFiles({ multiSig })
 
             if (shouldVerify(hre.network)) {
-                logger.out("Verifying the contract...")
+                logger.out("Verifying APTeamMultiSig...")
+    
+                await hre.run("verify:verify", {
+                    address: apMultisigImpl.address,
+                    constructorArguments: [],
+                    contract: "contracts/multisigs/APTeamMultiSig.sol:APTeamMultiSig"
+                })
+
+                logger.out("\nVerifying ApplicationsMultiSig...")
 
                 await hre.run("verify:verify", {
                     address: appsMultisigImpl.address,
                     constructorArguments: [],
-                })
-                await hre.run("verify:verify", {
-                    address: apMultisigImpl.address,
-                    constructorArguments: [],
+                    contract: "contracts/multisigs/ApplicationsMultiSig.sol:ApplicationsMultiSig"
                 })
             }
         } catch (error) {
