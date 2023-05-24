bbbb1f316df127e82b476271cffbf68ddc2409da
Nenad <n.misic@outlook.com>
2023-05-24 12:49:09 +0200

    Rearrange AddressObj fields

--- utils/manageAddressFile/types.ts
+++ utils/manageAddressFile/types.ts
@@ -1,9 +1,15 @@
 export type AddressObj = {
-    libraries: {
-        STRING_LIBRARY: string
-        ANGEL_CORE_STRUCT_LIBRARY: string
-    }
     accountsDiamond: string
+    charityApplication: {
+        proxy: string
+        implementation: string
+    }
+    EndowmentMultiSigAddress: {
+        EndowmentMultiSigEmitterProxy: string
+        EndowmentMultiSigEmitterImplementation: string
+        MultiSigWalletFactory: string
+        MultiSigWalletImplementation: string
+    }
     fundraising: {
         implementation: string
         library: string
@@ -17,51 +23,6 @@ export type AddressObj = {
         liquidVault: string
         lockedVault: string
     }
-    registrar: {
-        implementation: string
-        proxy: string
-    }
-    router: {
-        implementation: string
-        proxy: string
-    }
-    tokens: {
-        halo: string
-        usdc: string
-        weth: string
-    }
-    multiSig: {
-        ApplicationsMultiSigProxy: string
-        APTeamMultiSigProxy: string
-        ApplicationMultisigImplementation: string
-        APTeamMultisigImplementation: string
-    }
-    subDaoEmitter: {
-        proxy: string
-        implementation: string
-    }
-    charityApplication: {
-        proxy: string
-        implementation: string
-    }
-    swapRouterAddress2: {
-        proxy: string
-        implementation: string
-    }
-    indexFundAddress: {
-        proxy: string
-        implementation: string
-    }
-    EndowmentMultiSigAddress: {
-        EndowmentMultiSigEmitterProxy: string
-        EndowmentMultiSigEmitterImplementation: string
-        MultiSigWalletFactory: string
-        MultiSigWalletImplementation: string
-    }
-    lockedWithdraw: {
-        implementation: string
-        proxy: string
-    }
     HaloImplementations: {
         DonationMatchImplementation: string
         DonationMatchAddress: {
@@ -72,5 +33,44 @@ export type AddressObj = {
         subDaoERC20Implementation: string
         subDaoveTokenImplementation: string
         IncentivisedVotingLockupImplementation: string
+    }
+    indexFundAddress: {
+        proxy: string
+        implementation: string
+    }
+    libraries: {
+        STRING_LIBRARY: string
+        ANGEL_CORE_STRUCT_LIBRARY: string
+    }
+    lockedWithdraw: {
+        implementation: string
+        proxy: string
+    }
+    multiSig: {
+        ApplicationsMultiSigProxy: string
+        APTeamMultiSigProxy: string
+        ApplicationMultisigImplementation: string
+        APTeamMultisigImplementation: string
+    }
+    registrar: {
+        implementation: string
+        proxy: string
+    }
+    router: {
+        implementation: string
+        proxy: string
+    }
+    subDaoEmitter: {
+        proxy: string
+        implementation: string
+    }
+    swapRouterAddress2: {
+        proxy: string
+        implementation: string
+    }
+    tokens: {
+        halo: string
+        usdc: string
+        weth: string
     }
 }
