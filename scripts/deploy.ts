b759dcd9706c0e014afe12137b0c28848b3ba246
Nenad <n.misic@outlook.com>
2023-05-30 11:07:26 +0200

    Refactor deployLibraries

--- scripts/deploy.ts
+++
@@ -1,9 +0,0 @@
-import hre from "hardhat";
-import deploy from "./deployAngelProtocol";
-
-deploy(hre)
-  .then(() => process.exit(0))
-  .catch((error) => {
-    console.error(error);
-    process.exit(1);
-  });
