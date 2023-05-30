335d0d86e5e400af089c5f59c62f2c80eb455e95
Nenad <n.misic@outlook.com>
2023-05-30 06:40:07 +0200

    Refactor ParametersExceptLast

--- utils/types.ts
+++ utils/types.ts
@@ -1,9 +1,3 @@
-type Wrap<T> = {[K in keyof T]-?: [T[K]]};
-type Unwrap<T> = {[K in keyof T]: Extract<T[K], [any]>[0]};
+type Head<T extends any[]> = Required<T> extends [...infer Head, any] ? Head : never;
 
-export type ParametersExceptLast<F extends (...args: any) => any> = Wrap<Parameters<F>> extends [
-  ...infer InitPs,
-  any
-]
-  ? Unwrap<InitPs>
-  : never;
+export type ParametersExceptLast<F extends (...args: any) => any> = Head<Parameters<F>>;
