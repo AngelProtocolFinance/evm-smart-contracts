47001cdffefece232cf86edb625e4010a33883c9
Nenad <n.misic@outlook.com>
2023-05-31 10:08:33 +0200

    Update deployRegistrar task to redeploy the Router too

--- tasks/deploy/deployRegistrar.ts
+++ tasks/deploy/deployRegistrar.ts
@@ -1,7 +1,8 @@
 import {task} from "hardhat/config";
 import config from "config";
 import {deployRegistrar} from "contracts/core/registrar/scripts/deploy";
-import {getAddresses, getSigners, logger} from "utils";
+import {getAddresses, getSigners, isLocalNetwork, logger} from "utils";
+import {deployRouter} from "contracts/core/router/scripts/deploy";
 
 task("Deploy:Registrar", "Will deploy Registrar contract")
   .addParam("verify", "Want to verify contract")
@@ -12,6 +13,7 @@ task("Deploy:Registrar", "Will deploy Registrar contract")
         router,
       } = await getAddresses(hre);
       const {treasury} = await getSigners(hre.ethers);
+
       const registrarData = {
         treasury: treasury.address,
         splitToLiquid: config.REGISTRAR_DATA.splitToLiquid,
@@ -19,8 +21,18 @@ task("Deploy:Registrar", "Will deploy Registrar contract")
         axelarGateway: config.REGISTRAR_DATA.axelarGateway,
         axelarGasRecv: config.REGISTRAR_DATA.axelarGasRecv,
       };
-      var isTrueSet = taskArgs.verify === "true";
-      await deployRegistrar(registrarData, apTeam.proxy, isTrueSet, hre);
+      const verify_contracts = !isLocalNetwork(hre.network) && taskArgs.verify === "true";
+
+      const registrar = await deployRegistrar(registrarData, apTeam.proxy, verify_contracts, hre);
+
+      await deployRouter(
+        config.REGISTRAR_DATA.axelarGateway,
+        config.REGISTRAR_DATA.axelarGasRecv,
+        registrar.proxy.address,
+        apTeam.proxy,
+        verify_contracts,
+        hre
+      );
     } catch (error) {
       logger.out(error, logger.Level.Error);
     }
