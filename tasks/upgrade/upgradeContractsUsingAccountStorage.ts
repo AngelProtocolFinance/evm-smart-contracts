b7c7e93359bf0595b7b2bf6c8282efc63234ad51
Nenad <n.misic@outlook.com>
2023-06-01 10:20:36 +0200

    Fix upgradeFacets name in upgradeContractsUsingAccountStorage

--- tasks/upgrade/upgradeContractsUsingAccountStorage.ts
+++ tasks/upgrade/upgradeContractsUsingAccountStorage.ts
@@ -7,7 +7,7 @@ task(
 ).setAction(async (_taskArguments, hre) => {
   try {
     await hre.run("upgrade:upgradeCharityApplication");
-    await hre.run("upgrade:upgradeFacets", {facets: ["all"]});
+    await hre.run("upgrade:facets", {facets: ["all"]});
   } catch (error) {
     logger.out(
       `Redeployment of all contracts that use AccountStorage struct failed, reason: ${error}`,
