17154f84ba68889680d752c28cb2e9159afd8950
Nenad <n.misic@outlook.com>
2023-06-01 11:10:32 +0200

    Revert temp changes to deployAngelProtocol

--- tasks/deploy/deployAngelProtocol.ts
+++ tasks/deploy/deployAngelProtocol.ts
@@ -1,11 +1,10 @@
 import {task} from "hardhat/config";
 import {deployAngelProtocol} from "scripts";
-import {envConfig, logger} from "utils";
+import {logger} from "utils";
 
 task("Deploy:AngelProtocol", "Will deploy complete Angel Protocol").setAction(async (_, hre) => {
   try {
-    logger.out(envConfig.PROD_NETWORK_ID);
-    // await deployAngelProtocol(hre);
+    await deployAngelProtocol(hre);
   } catch (error) {
     logger.out(error, logger.Level.Error);
   } finally {
