894d49089ade5c5d977d976739fe39dd5554a8c6
Nenad <n.misic@outlook.com>
2023-06-09 09:10:22 +0200

    Fix deploy:CharityApplications

--- tasks/deploy/deployCharityApplications.ts
+++ tasks/deploy/deployCharityApplications.ts
@@ -1,35 +1,37 @@
-import config from "config";
 import {task, types} from "hardhat/config";
 import {isLocalNetwork, logger} from "utils";
 import {getAddresses} from "utils";
-
 import {deployCharityApplication} from "contracts/multisigs/charity_applications/scripts/deploy";
 
+type TaskArgs = {accountsDiamond?: string; applications?: string; verify: boolean};
+
 task("deploy:CharityApplications", "Will deploy CharityApplications contract")
+  .addOptionalParam(
+    "accountsDiamond",
+    "Address of the Accounts Diamond contract. Will do a local lookup from contract-address.json if none is provided."
+  )
+  .addOptionalParam(
+    "applications",
+    "Address of the ApplicationsMultiSig contract. Will do a local lookup from contract-address.json if none is provided."
+  )
   .addOptionalParam(
     "verify",
     "Indicates whether the contract should be verified",
     false,
     types.boolean
   )
-  .setAction(async (taskArgs: {verify: boolean}, hre) => {
+  .setAction(async (taskArgs: TaskArgs, hre) => {
     try {
       const addresses = await getAddresses(hre);
-      const charityApplicationsData: Parameters<typeof deployCharityApplication>[0] = [
-        config.CHARITY_APPLICATION_DATA.expiry,
-        addresses.multiSig.applications.proxy,
-        addresses.accounts.diamond,
-        config.CHARITY_APPLICATION_DATA.seedSplitToLiquid,
-        config.CHARITY_APPLICATION_DATA.newEndowGasMoney,
-        config.CHARITY_APPLICATION_DATA.gasAmount,
-        config.CHARITY_APPLICATION_DATA.fundSeedAsset,
-        config.CHARITY_APPLICATION_DATA.seedAsset,
-        config.CHARITY_APPLICATION_DATA.seedAssetAmount,
-      ];
+
+      const applications = taskArgs.applications || addresses.multiSig.applications.proxy;
+      const accountsDiamond = taskArgs.accountsDiamond || addresses.accounts.diamond;
       const verify_contracts = !isLocalNetwork(hre) && taskArgs.verify;
 
-      await deployCharityApplication(charityApplicationsData, verify_contracts, hre);
+      await deployCharityApplication(applications, accountsDiamond, verify_contracts, hre);
     } catch (error) {
       logger.out(error, logger.Level.Error);
+    } finally {
+      logger.out("Done.");
     }
   });
