a5d606273744bd81c2341065073f88ef1f487713
Nenad <n.misic@outlook.com>
2023-06-09 08:37:58 +0200

    Add better typing to deploySwapRouter

--- tasks/deploy/deploySwapRouter.ts
+++ tasks/deploy/deploySwapRouter.ts
@@ -1,10 +1,10 @@
 import config from "config";
 import {task, types} from "hardhat/config";
-import {isLocalNetwork, logger} from "utils";
+import {getAddresses, isLocalNetwork, logger} from "utils";
 
 import {deploySwapRouter} from "contracts/core/swap-router/scripts/deploy";
 
-type TaskArgs = {accountaddress: string; registraraddress: string; verify: boolean};
+type TaskArgs = {accountsDiamond?: string; registrar?: string; verify: boolean};
 
 task("deploy:SwapRouter", "Will deploy SwapRouter contract")
   .addOptionalParam(
@@ -13,15 +13,25 @@ task("deploy:SwapRouter", "Will deploy SwapRouter contract")
     false,
     types.boolean
   )
-  .addParam("registraraddress", "Address of the Registrar contract")
-  .addParam("accountaddress", "Address of the account")
+  .addOptionalParam(
+    "registrar",
+    "Address of the Registrar contract. Will do a local lookup from contract-address.json if none is provided."
+  )
+  .addOptionalParam(
+    "accountsDiamond",
+    "Address of the Accounts Diamond contract. Will do a local lookup from contract-address.json if none is provided."
+  )
   .setAction(async (taskArgs: TaskArgs, hre) => {
     try {
+      const addresses = await getAddresses(hre);
+
+      const registrar = taskArgs.registrar || addresses.registrar.proxy;
+      const accountsDiamond = taskArgs.accountsDiamond || addresses.accounts.diamond;
       const verify_contracts = !isLocalNetwork(hre) && taskArgs.verify;
 
       await deploySwapRouter(
-        taskArgs.registraraddress,
-        taskArgs.accountaddress,
+        registrar,
+        accountsDiamond,
         config.SWAP_ROUTER_DATA.SWAP_FACTORY_ADDRESS,
         config.SWAP_ROUTER_DATA.SWAP_ROUTER_ADDRESS,
         verify_contracts,
