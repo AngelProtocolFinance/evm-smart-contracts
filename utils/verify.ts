9a251bab453f30d883d9694388d8a192fa13c04b
Nenad <n.misic@outlook.com>
2023-06-12 12:09:49 +0200

    Refactor verify

--- utils/verify.ts
+++ utils/verify.ts
@@ -1,15 +1,31 @@
 import {HardhatRuntimeEnvironment} from "hardhat/types";
 import {logger} from ".";
+import {BaseContract} from "ethers";
 
-export async function verify(
-  hre: HardhatRuntimeEnvironment,
-  address: string,
-  constructorArguments: any[] = []
-) {
+type Args =
+  | {
+      contract: BaseContract;
+      constructorArguments?: any[];
+      contractPath?: string;
+      address?: never; // Ensures `address` property cannot be present
+    }
+  | {
+      address: string;
+      constructorArguments?: any[];
+      contractPath?: string;
+      contract?: never; // Ensures `contract` property cannot be present
+    };
+
+export async function verify(hre: HardhatRuntimeEnvironment, args: Args) {
+  const contractName = args.address === undefined ? args.contract.constructor.name : "contract";
+  const address = args.address === undefined ? args.contract.address : args.address;
+
   try {
+    logger.out(`Verifying ${contractName} at: ${address}...`);
     await hre.run("verify:verify", {
       address,
-      constructorArguments,
+      constructorArguments: args.constructorArguments,
+      contract: args.contractPath,
     });
   } catch (error) {
     logger.out(error, logger.Level.Error);
