23f6c2a6afbf346f605eeac2f2f598a8368306d4
Nenad <n.misic@outlook.com>
2023-06-12 10:55:21 +0200

    Update confirmAction message

--- utils/confirmAction.ts
+++ utils/confirmAction.ts
@@ -2,6 +2,8 @@ import {createInterface} from "node:readline/promises";
 
 export async function confirmAction(description: string): Promise<boolean> {
   const readline = createInterface({input: process.stdin, output: process.stdout});
-  const answer = await readline.question(`${description}\nAre you sure you wish to do this? (Y/y)`);
+  const answer = await readline.question(
+    `${description}\nAre you sure you wish to perform this action? (Y/y)`
+  );
   return /^(|y|yes)$/i.test(answer);
 }
