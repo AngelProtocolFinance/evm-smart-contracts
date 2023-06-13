d9716d4566f4c6118fafde8eae3b49c46b225eb2
Andrey <SovereignAndrey>
2023-06-13 14:29:44 +0800

    add missing contracts; yarn format config; only compile is clean-compile

--- eth-sdk/eth-sdk.config.ts
+++ eth-sdk/eth-sdk.config.ts
@@ -1,19 +1,18 @@
-import { defineConfig } from "@dethcrypto/eth-sdk";
-import type { Opaque } from 'ts-essentials'
-import type { ZodTypeDef } from 'zod'
-import { z } from 'zod'
+import {defineConfig} from "@dethcrypto/eth-sdk";
+import type {Opaque} from "ts-essentials";
+import type {ZodTypeDef} from "zod";
+import {z} from "zod";
 
-import { DEFAULT_CONTRACT_ADDRESS_FILE_PATH } from "../utils/constants";
-import { readAllAddresses, getAddressesByNetworkId } from "../utils/manageAddressFile/helpers";
-import { AddressObj } from "../utils/manageAddressFile/types";
+import {DEFAULT_CONTRACT_ADDRESS_FILE_PATH} from "../utils/constants";
+import {readAllAddresses, getAddressesByNetworkId} from "../utils/manageAddressFile/helpers";
+import {AddressObj} from "../utils/manageAddressFile/types";
 
-
-type AddressInput = `0x${string}`
-type Address = Opaque<AddressInput, 'Address'>
+type AddressInput = `0x${string}`;
+type Address = Opaque<AddressInput, "Address">;
 const addressSchema: z.ZodType<Address, ZodTypeDef, AddressInput> = z
   .string()
-  .length(42, { message: 'An address must be 42 characters long' })
-  .regex(/^0x[0-9a-fA-F]+$/, { message: 'An address must be a hexadecimal number string' }) as any
+  .length(42, {message: "An address must be 42 characters long"})
+  .regex(/^0x[0-9a-fA-F]+$/, {message: "An address must be a hexadecimal number string"}) as any;
 
 /**
  * @see https://info.etherscan.com/what-is-an-ethereum-address/
@@ -21,11 +20,11 @@ const addressSchema: z.ZodType<Address, ZodTypeDef, AddressInput> = z
  * @returns the same string branded as Address if it's a valid address
  */
 export function parseAddress(address: string): Address {
-  const res = addressSchema.safeParse(address)
-  if (res.success) return res.data
+  const res = addressSchema.safeParse(address);
+  if (res.success) return res.data;
   else {
-    const errorCode = res.error.issues[0].code
-    throw new Error(`"${address}" is not a valid address.`)
+    const errorCode = res.error.issues[0].code;
+    throw new Error(`"${address}" is not a valid address.`);
   }
 }
 
@@ -37,87 +36,94 @@ const mumbaiData: AddressObj = getAddressesByNetworkId(80001, DEFAULT_CONTRACT_A
 const polygonData: AddressObj = getAddressesByNetworkId(137, DEFAULT_CONTRACT_ADDRESS_FILE_PATH);
 
 export default defineConfig({
-  	contracts: {
-		polygonMumbai: {
-		    accounts: {
-				diamond: parseAddress(mumbaiData.accounts.diamond),
-				facets: {
-					accountDeployContract: parseAddress(mumbaiData.accounts.facets.accountDeployContract),
-					accountDepositWithdrawEndowments: parseAddress(mumbaiData.accounts.facets.accountDepositWithdrawEndowments),
-					accountDonationMatch: parseAddress(mumbaiData.accounts.facets.accountDonationMatch),
-					accountsAllowance: parseAddress(mumbaiData.accounts.facets.accountsAllowance),
-					accountsCreateEndowment: parseAddress(mumbaiData.accounts.facets.accountsCreateEndowment),
-					accountsDAOEndowments: parseAddress(mumbaiData.accounts.facets.accountsDAOEndowments),
-					accountsQueryEndowments: parseAddress(mumbaiData.accounts.facets.accountsQueryEndowments),
-					accountsSwapEndowments: parseAddress(mumbaiData.accounts.facets.accountsSwapEndowments),
-					accountsUpdate: parseAddress(mumbaiData.accounts.facets.accountsUpdate),
-					accountsUpdateEndowments: parseAddress(mumbaiData.accounts.facets.accountsUpdateEndowments),
-					accountsUpdateEndowmentSettingsController: parseAddress(mumbaiData.accounts.facets.accountsUpdateEndowmentSettingsController),
-					accountsUpdateStatusEndowments: parseAddress(mumbaiData.accounts.facets.accountsUpdateStatusEndowments),
-					accountsVaultFacet: parseAddress(mumbaiData.accounts.facets.accountsVaultFacet),
-					diamondCutFacet: parseAddress(mumbaiData.accounts.facets.diamondCutFacet),
-					diamondInitFacet: parseAddress(mumbaiData.accounts.facets.diamondInitFacet),
-					diamondLoupeFacet: parseAddress(mumbaiData.accounts.facets.diamondLoupeFacet),
-					ownershipFacet: parseAddress(mumbaiData.accounts.facets.ownershipFacet),
-				},
-			},
-			charityApplication: {
-		      	implementation: parseAddress(mumbaiData.charityApplication.implementation),
-		    },
-		    donationMatch: {
-		      	implementation: parseAddress(mumbaiData.donationMatch.implementation),
-		    },
-		    donationMatchCharity: {
-				implementation: parseAddress(mumbaiData.donationMatchCharity.implementation),
-				proxy: parseAddress(mumbaiData.donationMatchCharity.proxy),
-		    },
-		    incentivisedVotingLockup: {
-		      	implementation: parseAddress(mumbaiData.incentivisedVotingLockup.implementation),
-		    },
-		    indexFund: {
-			      implementation: parseAddress(mumbaiData.indexFund.implementation),
-			      proxy: parseAddress(mumbaiData.indexFund.proxy),
-		    },
-		    libraries: {
-			      angelCoreStructLibrary: parseAddress(mumbaiData.libraries.ANGEL_CORE_STRUCT_LIBRARY),
-			      stringLibrary: parseAddress(mumbaiData.libraries.STRING_LIBRARY),
-		    },
-		    multiSig: {
-		      	applications: {
-		        	implementation: parseAddress(mumbaiData.multiSig.applications.implementation),
-		        	proxy: parseAddress(mumbaiData.multiSig.applications.proxy),
-		      	},
-		      	apTeam: {
-		        	implementation: parseAddress(mumbaiData.multiSig.apTeam.implementation),
-		        	proxy: parseAddress(mumbaiData.multiSig.apTeam.proxy),
-		      	},
-		      	endowment: {
-		        	emitter: {
-			          	implementation: parseAddress(mumbaiData.multiSig.endowment.emitter.implementation),
-			          	proxy: parseAddress(mumbaiData.multiSig.endowment.emitter.proxy),
-	        		},
-		        	factory: parseAddress(mumbaiData.multiSig.endowment.factory),
-		        	implementation: parseAddress(mumbaiData.multiSig.endowment.implementation),
-		     	},
-		    },
-		    registrar: {
-		      	implementation: parseAddress(mumbaiData.registrar.implementation),
-		      	proxy: parseAddress(mumbaiData.registrar.proxy),
-		    },
-		    subDao: {
-		      	emitter: {
-		        	implementation: parseAddress(mumbaiData.subDao.emitter.implementation),
-		        	proxy: parseAddress(mumbaiData.subDao.emitter.proxy),
-		      	},
-				implementation: parseAddress(mumbaiData.subDao.implementation),
-				token: parseAddress(mumbaiData.subDao.token),
-				veBondingToken: parseAddress(mumbaiData.subDao.veBondingToken),
-		    },
-		},
-		polygon: {},
-	},
-	rpc: {
-    	polygonMumbai: "https://api-testnet.polygonscan.com/",
-		polygon: "https://api.polygonscan.com/",
-  	},
+  contracts: {
+    polygonMumbai: {
+      accounts: {
+        diamond: parseAddress(mumbaiData.accounts.diamond),
+        facets: {
+          accountDeployContract: parseAddress(mumbaiData.accounts.facets.accountDeployContract),
+          accountDepositWithdrawEndowments: parseAddress(
+            mumbaiData.accounts.facets.accountDepositWithdrawEndowments
+          ),
+          accountDonationMatch: parseAddress(mumbaiData.accounts.facets.accountDonationMatch),
+          accountsAllowance: parseAddress(mumbaiData.accounts.facets.accountsAllowance),
+          accountsCreateEndowment: parseAddress(mumbaiData.accounts.facets.accountsCreateEndowment),
+          accountsDAOEndowments: parseAddress(mumbaiData.accounts.facets.accountsDAOEndowments),
+          accountsQueryEndowments: parseAddress(mumbaiData.accounts.facets.accountsQueryEndowments),
+          accountsSwapEndowments: parseAddress(mumbaiData.accounts.facets.accountsSwapEndowments),
+          accountsUpdate: parseAddress(mumbaiData.accounts.facets.accountsUpdate),
+          accountsUpdateEndowments: parseAddress(
+            mumbaiData.accounts.facets.accountsUpdateEndowments
+          ),
+          accountsUpdateEndowmentSettingsController: parseAddress(
+            mumbaiData.accounts.facets.accountsUpdateEndowmentSettingsController
+          ),
+          accountsUpdateStatusEndowments: parseAddress(
+            mumbaiData.accounts.facets.accountsUpdateStatusEndowments
+          ),
+          accountsVaultFacet: parseAddress(mumbaiData.accounts.facets.accountsVaultFacet),
+          diamondCutFacet: parseAddress(mumbaiData.accounts.facets.diamondCutFacet),
+          diamondInitFacet: parseAddress(mumbaiData.accounts.facets.diamondInitFacet),
+          diamondLoupeFacet: parseAddress(mumbaiData.accounts.facets.diamondLoupeFacet),
+          ownershipFacet: parseAddress(mumbaiData.accounts.facets.ownershipFacet),
+        },
+      },
+      charityApplication: {
+        implementation: parseAddress(mumbaiData.charityApplication.implementation),
+        proxy: parseAddress(mumbaiData.charityApplication.proxy),
+      },
+      donationMatch: {
+        implementation: parseAddress(mumbaiData.donationMatch.implementation),
+        proxy: parseAddress(mumbaiData.donationMatch.proxy),
+      },
+      donationMatchCharity: {
+        implementation: parseAddress(mumbaiData.donationMatchCharity.implementation),
+        proxy: parseAddress(mumbaiData.donationMatchCharity.proxy),
+      },
+      indexFund: {
+        implementation: parseAddress(mumbaiData.indexFund.implementation),
+        proxy: parseAddress(mumbaiData.indexFund.proxy),
+      },
+      libraries: {
+        angelCoreStructLibrary: parseAddress(mumbaiData.libraries.ANGEL_CORE_STRUCT_LIBRARY),
+        stringLibrary: parseAddress(mumbaiData.libraries.STRING_LIBRARY),
+      },
+      multiSig: {
+        applications: {
+          implementation: parseAddress(mumbaiData.multiSig.applications.implementation),
+          proxy: parseAddress(mumbaiData.multiSig.applications.proxy),
+        },
+        apTeam: {
+          implementation: parseAddress(mumbaiData.multiSig.apTeam.implementation),
+          proxy: parseAddress(mumbaiData.multiSig.apTeam.proxy),
+        },
+        endowment: {
+          emitter: {
+            implementation: parseAddress(mumbaiData.multiSig.endowment.emitter.implementation),
+            proxy: parseAddress(mumbaiData.multiSig.endowment.emitter.proxy),
+          },
+          factory: parseAddress(mumbaiData.multiSig.endowment.factory),
+          implementation: parseAddress(mumbaiData.multiSig.endowment.implementation),
+        },
+      },
+      registrar: {
+        implementation: parseAddress(mumbaiData.registrar.implementation),
+        proxy: parseAddress(mumbaiData.registrar.proxy),
+      },
+      subDao: {
+        emitter: {
+          implementation: parseAddress(mumbaiData.subDao.emitter.implementation),
+          proxy: parseAddress(mumbaiData.subDao.emitter.proxy),
+        },
+        implementation: parseAddress(mumbaiData.subDao.implementation),
+        token: parseAddress(mumbaiData.subDao.token),
+        veBondingToken: parseAddress(mumbaiData.subDao.veBondingToken),
+      },
+    },
+    polygon: {},
+  },
+  rpc: {
+    polygonMumbai: "https://api-testnet.polygonscan.com/",
+    polygon: "https://api.polygonscan.com/",
+  },
 });
