0b0cd44794342c3c7598f463e7e1f88536ce611b
Nenad <n.misic@outlook.com>
2023-06-15 19:47:04 +0200

    Make 'no' the default option in confirmAction

--- utils/confirmAction.ts
+++ utils/confirmAction.ts
@@ -3,7 +3,7 @@ import {createInterface} from "node:readline/promises";
 export async function confirmAction(actionDescription: string): Promise<boolean> {
   const readline = createInterface({input: process.stdin, output: process.stdout});
   const answer = await readline.question(
-    `${actionDescription}\nAre you sure you wish to perform this action? (Y/y)`
+    `${actionDescription}\nAre you sure you wish to perform this action? (y/N)`
   );
-  return /^(|y|yes)$/i.test(answer);
+  return /^(y|yes)$/i.test(answer);
 }
