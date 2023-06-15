6036cd36707e2eb74d43116df7b69ab51a5492ad
Nenad <n.misic@outlook.com>
2023-06-15 19:49:36 +0200

    Trim confirmAction input before testing

--- utils/confirmAction.ts
+++ utils/confirmAction.ts
@@ -2,8 +2,8 @@ import {createInterface} from "node:readline/promises";
 
 export async function confirmAction(actionDescription: string): Promise<boolean> {
   const readline = createInterface({input: process.stdin, output: process.stdout});
-  const answer = await readline.question(
-    `${actionDescription}\nAre you sure you wish to perform this action? (y/N)`
-  );
+  const answer = await readline
+    .question(`${actionDescription}\nAre you sure you wish to perform this action? (y/N)`)
+    .then((input) => input.trim());
   return /^(y|yes)$/i.test(answer);
 }
