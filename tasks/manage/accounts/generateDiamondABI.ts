13bb585cb713effa5977ba911212286b71c78036
Nenad <n.misic@outlook.com>
2023-06-19 15:48:12 +0200

    Update task name

--- tasks/manage/accounts/generateDiamondABI.ts
+++ tasks/manage/accounts/generateDiamondABI.ts
@@ -7,23 +7,24 @@ const angelCoreStruct = "./artifacts/contracts/core/struct.sol/AngelCoreStruct.j
 const basePath = "/contracts/core/accounts/facets/";
 const diamondAbiPath = "./diamondABI/diamond.json";
 
-task("diamondABI", "Generates ABI file for diamond, includes all ABIs of facets").setAction(
-  async () => {
-    let files = fs.readdirSync("." + basePath);
-    let abi: AbiCoder[] = [];
-    for (const file of files) {
-      const jsonFile = file.replace("sol", "json");
-      const json = fs.readFileSync(`./artifacts/${basePath}${file}/${jsonFile}`, "utf-8");
-      const obj = JSON.parse(json) as any;
-      abi.push(...obj.abi);
-    }
-    for (const file of files) {
-      const json = fs.readFileSync(angelCoreStruct, "utf-8");
-      const obj = JSON.parse(json) as any;
-      abi.push(...obj.abi);
-    }
-    const finalAbi = JSON.stringify(abi, undefined, 2);
-    fs.writeFileSync(diamondAbiPath, finalAbi);
-    logger.out("ABI written to diamondABI/diamond.json");
+task(
+  "manage:accounts:generateDiamondABI",
+  "Generates ABI file for diamond, includes all ABIs of facets"
+).setAction(async () => {
+  let files = fs.readdirSync("." + basePath);
+  let abi: AbiCoder[] = [];
+  for (const file of files) {
+    const jsonFile = file.replace("sol", "json");
+    const json = fs.readFileSync(`./artifacts/${basePath}${file}/${jsonFile}`, "utf-8");
+    const obj = JSON.parse(json) as any;
+    abi.push(...obj.abi);
   }
-);
+  for (const file of files) {
+    const json = fs.readFileSync(angelCoreStruct, "utf-8");
+    const obj = JSON.parse(json) as any;
+    abi.push(...obj.abi);
+  }
+  const finalAbi = JSON.stringify(abi, undefined, 2);
+  fs.writeFileSync(diamondAbiPath, finalAbi);
+  logger.out("ABI written to diamondABI/diamond.json");
+});
