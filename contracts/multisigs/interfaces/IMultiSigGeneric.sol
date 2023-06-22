fc61d0a540a6d306b878991ba396c121954cd72d
Nenad <n.misic@outlook.com>
2023-06-22 08:10:45 +0200

    Remove unused imports from IMultiSigGeneric

--- contracts/multisigs/interfaces/IMultiSigGeneric.sol
+++ contracts/multisigs/interfaces/IMultiSigGeneric.sol
@@ -1,7 +1,6 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.16;
 
-import {MultiSigStorage} from "../storage.sol";
 import "@openzeppelin/contracts/utils/introspection/IERC165.sol";
 
 abstract contract IMultiSigGeneric is IERC165 {
