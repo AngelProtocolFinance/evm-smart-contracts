55970379c9e81f8847cb4969440cf7d4e74413c7
Nenad <n.misic@outlook.com>
2023-06-22 11:54:54 +0200

    Update IRouter events to passive form

--- contracts/core/router/IRouter.sol
+++ contracts/core/router/IRouter.sol
@@ -10,13 +10,13 @@ interface IRouter is IAxelarExecutable {
                         EVENTS
     */ ////////////////////////////////////////////////
 
-  event TokensSent(IVault.VaultActionData action, uint256 amount);
-  event FallbackRefund(IVault.VaultActionData action, uint256 amount);
-  event Deposit(IVault.VaultActionData action);
-  event Redemption(IVault.VaultActionData action, uint256 amount);
-  event Harvest(IVault.VaultActionData action);
-  event LogError(IVault.VaultActionData action, string message);
-  event LogErrorBytes(IVault.VaultActionData action, bytes data);
+  event TokenSent(IVault.VaultActionData action, uint256 amount);
+  event TokenRefunded(IVault.VaultActionData action, uint256 amount);
+  event TokenDeposited(IVault.VaultActionData action);
+  event TokenRedeemed(IVault.VaultActionData action, uint256 amount);
+  event RewardsHarvested(IVault.VaultActionData action);
+  event ErrorLogged(IVault.VaultActionData action, string message);
+  event ErrorBytesLogged(IVault.VaultActionData action, bytes data);
 
   /*////////////////////////////////////////////////
                     CUSTOM TYPES
