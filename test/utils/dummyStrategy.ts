7e0251932d6f31b526439f18ea3cf19d984e7db4
Nenad <n.misic@outlook.com>
2023-06-30 10:12:26 +0200

    Fix dummyStrategy

--- test/utils/dummyStrategy.ts
+++ test/utils/dummyStrategy.ts
@@ -1,5 +1,4 @@
 import {SignerWithAddress} from "@nomiclabs/hardhat-ethers/signers";
-import {ADDRESS_ZERO} from "utils";
 import {DummyStrategy, DummyStrategy__factory, IStrategy} from "typechain-types";
 import {DEFAULT_STRATEGY_SELECTOR} from "./constants";
 
@@ -10,15 +9,11 @@ export async function deployDummyStrategy(
     yieldToken,
     admin,
     strategySelector = DEFAULT_STRATEGY_SELECTOR,
-    lockedVault = ADDRESS_ZERO,
-    liquidVault = ADDRESS_ZERO,
   }: {
     baseToken: string;
     yieldToken: string;
     admin: string;
     strategySelector?: string;
-    lockedVault?: string;
-    liquidVault?: string;
   }
 ): Promise<DummyStrategy> {
   let Strategy = new DummyStrategy__factory(deployer);
@@ -27,8 +22,6 @@ export async function deployDummyStrategy(
     yieldToken: yieldToken,
     admin: admin,
     strategySelector: strategySelector,
-    lockedVault: lockedVault,
-    liquidVault: liquidVault,
   };
   const strategy = await Strategy.deploy(stratInitConfig);
   await strategy.deployed();
