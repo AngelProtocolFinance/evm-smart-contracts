50700c4a5c1b69fb28d811bd3b97fb3dc53e8259
Nenad <n.misic@outlook.com>
2023-07-03 09:45:48 +0200

    Set configuration in deployFacetAsProxy

--- test/core/accounts/utils/deployTestFacet.ts
+++ test/core/accounts/utils/deployTestFacet.ts
@@ -1,6 +1,8 @@
 import {SignerWithAddress} from "@nomiclabs/hardhat-ethers/signers";
 import {HardhatRuntimeEnvironment} from "hardhat/types";
 import {TestFacetProxyContract__factory, TestFacetProxyContract} from "typechain-types";
+import {ADDRESS_ZERO} from "utils";
+
 export async function deployFacetAsProxy(
   hre: HardhatRuntimeEnvironment,
   deployer: SignerWithAddress,
@@ -10,5 +12,17 @@ export async function deployFacetAsProxy(
   let Proxy = new TestFacetProxyContract__factory(deployer);
   let proxy = await Proxy.deploy(implementation, proxyAdmin.address, []);
   await proxy.deployed();
+  await proxy.setConfig({
+    owner: deployer.address,
+    version: "1",
+    registrarContract: ADDRESS_ZERO,
+    nextAccountId: 1,
+    maxGeneralCategoryId: 1,
+    subDao: ADDRESS_ZERO,
+    gateway: ADDRESS_ZERO,
+    gasReceiver: ADDRESS_ZERO,
+    earlyLockedWithdrawFee: {bps: 1000, payoutAddress: ADDRESS_ZERO},
+    reentrancyGuardLocked: false,
+  });
   return proxy;
 }
