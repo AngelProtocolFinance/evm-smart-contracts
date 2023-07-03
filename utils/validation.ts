00aa6dbe26163ca7afa2f32e3e9390f3f518ccd9
Nenad <n.misic@outlook.com>
2023-07-03 15:05:14 +0200

    Add AccountsCreateEndowment test cases

--- utils/validation.ts
+++ utils/validation.ts
@@ -1,7 +1,8 @@
 import {ADDRESS_ZERO} from "./constants";
 
-export function validateAddress(address: string, fieldName: string) {
+export function validateAddress(address: string | undefined, fieldName: string): address is string {
   if (!address || address === ADDRESS_ZERO) {
     throw new Error(`Invalid "${fieldName}" value: "${address}"`);
   }
+  return true;
 }
