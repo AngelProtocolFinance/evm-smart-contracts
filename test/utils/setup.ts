57dae61b3917aecb917668d27bec10885ceb460b
Nenad <n.misic@outlook.com>
2023-07-04 12:10:01 +0200

    Add chai method extension

--- test/utils/setup.ts
+++ test/utils/setup.ts
@@ -1,4 +1,7 @@
 import {logger} from "utils";
+import {LibAccounts} from "typechain-types/contracts/test/accounts/TestFacetProxyContract";
+import {Assertion, util} from "chai";
+import {BigNumber} from "ethers";
 
 before(() => {
   logger.off();
@@ -6,4 +9,10 @@ before(() => {
 
 after(() => {
   logger.on();
+});
+
+Assertion.addMethod("equalFee", function (fee: LibAccounts.FeeSettingStruct) {
+  var obj: LibAccounts.FeeSettingStructOutput = util.flag(this, "object");
+  new Assertion(obj.bps).to.equal(BigNumber.from(fee.bps));
+  new Assertion(obj.payoutAddress).to.equal(fee.payoutAddress);
 });
