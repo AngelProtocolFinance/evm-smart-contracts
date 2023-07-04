9b2b1e5c914ae25e76378e3431d1d01d2fd4cb8e
Nenad <n.misic@outlook.com>
2023-07-04 13:16:12 +0200

    Update equalFee to equalStruct

--- types/chai.d.ts
+++ types/chai.d.ts
@@ -2,6 +2,6 @@ declare module Chai {
   import {LibAccounts} from "typechain-types/contracts/test/accounts/TestFacetProxyContract";
 
   export interface Assertion {
-    equalFee(fee: LibAccounts.FeeSettingStruct): Assertion;
+    equalStruct(struct: any): Assertion;
   }
 }
