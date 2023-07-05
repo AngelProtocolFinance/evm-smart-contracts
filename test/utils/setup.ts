a33df4212adefe5d72b14dba581c1046b39395c3
Nenad <n.misic@outlook.com>
2023-07-05 07:17:58 +0200

    Revert "Update equalFee to equalStruct"
    This reverts commit 50a9993abfdf33a91885d99a40ec84b25c476d1b.

--- test/utils/setup.ts
+++ test/utils/setup.ts
@@ -1,4 +1,5 @@
 import {logger} from "utils";
+import {LibAccounts} from "typechain-types/contracts/test/accounts/TestFacetProxyContract";
 import {Assertion, util} from "chai";
 import {BigNumber} from "ethers";
 
@@ -10,17 +11,8 @@ after(() => {
   logger.on();
 });
 
-Assertion.addMethod("equalStruct", function (struct: any) {
-  var obj = util.flag(this, "object");
-  Object.keys(obj)
-    .filter((key) => isNaN(Number(key)))
-    .forEach((key) => {
-      const structVal = typeof struct[key] === "number" ? BigNumber.from(struct[key]) : struct[key];
-      new Assertion(obj[key]).to.equal(structVal);
-    });
-  // we need to check whether `struct` has the same fields as `obj`
-  Object.keys(struct).forEach((key) => {
-    const structVal = typeof struct[key] === "number" ? BigNumber.from(struct[key]) : struct[key];
-    new Assertion(obj[key]).to.equal(structVal);
-  });
+Assertion.addMethod("equalFee", function (fee: LibAccounts.FeeSettingStruct) {
+  var obj: LibAccounts.FeeSettingStructOutput = util.flag(this, "object");
+  new Assertion(obj.bps).to.equal(BigNumber.from(fee.bps));
+  new Assertion(obj.payoutAddress).to.equal(fee.payoutAddress);
 });
--- types/chai.d.ts
+++ types/chai.d.ts
@@ -2,6 +2,6 @@ declare module Chai {
   import {LibAccounts} from "typechain-types/contracts/test/accounts/TestFacetProxyContract";
 
   export interface Assertion {
-    equalStruct(struct: any): Assertion;
+    equalFee(fee: LibAccounts.FeeSettingStruct): Assertion;
   }
 }
