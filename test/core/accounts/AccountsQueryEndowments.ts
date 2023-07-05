10d6ea240cfceef68d087103fc9f7855ade32cc2
Nenad <n.misic@outlook.com>
2023-07-05 08:20:08 +0200

    Use equalFee in queryConfig test

--- test/core/accounts/AccountsQueryEndowments.ts
+++ test/core/accounts/AccountsQueryEndowments.ts
@@ -235,10 +235,7 @@ describe("AccountsQueryEndowments", function () {
       const configResponse = await facet.queryConfig();
 
       // Assert the expected config
-      expect(configResponse.earlyLockedWithdrawFee.bps).to.equal(config.earlyLockedWithdrawFee.bps);
-      expect(configResponse.earlyLockedWithdrawFee.payoutAddress).to.equal(
-        config.earlyLockedWithdrawFee.payoutAddress
-      );
+      expect(configResponse.earlyLockedWithdrawFee).to.equalFee(config.earlyLockedWithdrawFee);
       expect(configResponse.gasReceiver).to.equal(config.gasReceiver);
       expect(configResponse.gateway).to.equal(config.gateway);
       expect(configResponse.maxGeneralCategoryId).to.equal(config.maxGeneralCategoryId);
