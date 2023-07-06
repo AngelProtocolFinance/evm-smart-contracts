af6bbf4b08aec01bf9daed46084891a22d2ef6fe
Nenad <n.misic@outlook.com>
2023-07-06 13:06:54 +0200

    Add new test case and fix previous one

--- test/core/accounts/AccountsUpdateEndowmentSettingsController.ts
+++ test/core/accounts/AccountsUpdateEndowmentSettingsController.ts
@@ -13,6 +13,7 @@ import "../../utils/setup";
 import {DEFAULT_CHARITY_ENDOWMENT} from "test/utils";
 import {AccountStorage} from "typechain-types/contracts/test/accounts/TestFacetProxyContract";
 import {genWallet} from "utils";
+import {BigNumber} from "ethers";
 
 use(smock.matchers);
 
@@ -23,12 +24,14 @@ describe("AccountsUpdateEndowmentSettingsController", function () {
 
   let owner: SignerWithAddress;
   let proxyAdmin: SignerWithAddress;
+  let endowOwner: SignerWithAddress;
   let facet: AccountsUpdateEndowmentSettingsController;
   let state: TestFacetProxyContract;
+  let oldEndowment: AccountStorage.EndowmentStruct;
 
   before(async function () {
     let signers: SignerWithAddress[];
-    [owner, proxyAdmin, ...signers] = await ethers.getSigners();
+    [owner, proxyAdmin, endowOwner, ...signers] = await ethers.getSigners();
   });
 
   beforeEach(async function () {
@@ -49,7 +52,14 @@ describe("AccountsUpdateEndowmentSettingsController", function () {
       reentrancyGuardLocked: false,
     });
 
-    facet = AccountsUpdateEndowmentSettingsController__factory.connect(state.address, owner);
+    oldEndowment = {
+      ...DEFAULT_CHARITY_ENDOWMENT,
+      endowType: 1,
+      owner: endowOwner.address,
+    };
+    await state.setEndowmentDetails(endowId, oldEndowment);
+
+    facet = AccountsUpdateEndowmentSettingsController__factory.connect(state.address, endowOwner);
   });
 
   describe("updateEndowmentSettings", () => {
@@ -60,12 +70,12 @@ describe("AccountsUpdateEndowmentSettingsController", function () {
         id: endowId,
         allowlistedBeneficiaries: [genWallet().address],
         allowlistedContributors: [genWallet().address],
-        donationMatchActive: false,
-        ignoreUserSplits: false,
+        donationMatchActive: true,
+        ignoreUserSplits: true,
         maturity_allowlist_add: [genWallet().address],
         maturity_allowlist_remove: [genWallet().address],
-        maturityTime: 0,
-        splitToLiquid: {defaultSplit: 50, max: 100, min: 0},
+        maturityTime: 100,
+        splitToLiquid: {defaultSplit: 40, max: 80, min: 20},
       };
     });
 
@@ -80,19 +90,44 @@ describe("AccountsUpdateEndowmentSettingsController", function () {
     });
 
     it("reverts if a normal endowment is updating its maturityAllowlist with a zero address value", async () => {
-      const details: AccountStorage.EndowmentStruct = {
-        ...DEFAULT_CHARITY_ENDOWMENT,
-        endowType: 1,
-        maturityTime: updateEndowmentSettingsRequest.maturityTime,
-        owner: owner.address,
+      const request: AccountMessages.UpdateEndowmentSettingsRequestStruct = {
+        ...updateEndowmentSettingsRequest,
+        maturityTime: 0,
+        maturity_allowlist_add: [ethers.constants.AddressZero],
       };
-      await state.setEndowmentDetails(endowId, details);
 
-      updateEndowmentSettingsRequest.maturity_allowlist_add = [ethers.constants.AddressZero];
+      await expect(facet.updateEndowmentSettings(request)).to.be.revertedWith("InvalidAddress");
+    });
 
-      await expect(
-        facet.updateEndowmentSettings(updateEndowmentSettingsRequest)
-      ).to.be.revertedWith("InvalidAddress");
+    it("changes nothing in charity settings if sender doesn't have necessary permissions", async () => {
+      const tx = await facet.connect(owner).updateEndowmentSettings(updateEndowmentSettingsRequest);
+      const createEndowmentReceipt = await tx.wait();
+
+      // Get the endowment ID from the event emitted in the transaction receipt
+      const event = createEndowmentReceipt.events?.find((e) => e.event === "EndowmentUpdated");
+
+      // verify endowment was created by checking the emitted event's parameter
+      expect(event).to.exist;
+      expect(event?.args).to.exist;
+      expect(BigNumber.from(event!.args!.endowId)).to.equal(endowId);
+
+      const newEndow = await state.getEndowmentDetails(endowId);
+
+      expect(newEndow.allowlistedBeneficiaries).to.have.same.members(
+        oldEndowment.allowlistedBeneficiaries.map((x) => x.toString())
+      );
+      expect(newEndow.allowlistedContributors).to.have.same.members(
+        oldEndowment.allowlistedContributors.map((x) => x.toString())
+      );
+      expect(newEndow.donationMatchActive).to.equal(oldEndowment.donationMatchActive);
+      expect(newEndow.ignoreUserSplits).to.equal(oldEndowment.ignoreUserSplits);
+      expect(newEndow.maturityAllowlist).to.have.same.members(
+        oldEndowment.maturityAllowlist.map((x) => x.toString())
+      );
+      expect(newEndow.maturityTime).to.equal(oldEndowment.maturityTime);
+      expect(newEndow.splitToLiquid.defaultSplit).to.equal(oldEndowment.splitToLiquid.defaultSplit);
+      expect(newEndow.splitToLiquid.max).to.equal(oldEndowment.splitToLiquid.max);
+      expect(newEndow.splitToLiquid.min).to.equal(oldEndowment.splitToLiquid.min);
     });
   });
 });
