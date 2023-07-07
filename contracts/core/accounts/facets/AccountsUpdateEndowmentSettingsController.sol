40db924040f5512f8357d60fe6c3f65bc8e78fc6
Nenad <n.misic@outlook.com>
2023-07-07 11:18:46 +0200

    Update comment

--- contracts/core/accounts/facets/AccountsUpdateEndowmentSettingsController.sol
+++ contracts/core/accounts/facets/AccountsUpdateEndowmentSettingsController.sol
@@ -66,7 +66,7 @@ contract AccountsUpdateEndowmentSettingsController is
           tempEndowment.allowlistedBeneficiaries = details.allowlistedBeneficiaries;
           // IS EMITTING THIS EVENT FOR EACH UPDATED FIELD A GENERAL PRACTICE?
           // I SEE THE BENEFIT, JUST NOT SURE ABOUT GAS EFFICIENCY AND USEFULNESS
-          // from: Nenad -> does it even make sense to log every individual field that's changed
+          // from: Nenad -> does it even make sense to log every individual field that's the function is changing
           // when we still emit `EndowmentUpdated(endowId)` at the end of the func?
           emit EndowmentSettingUpdated(details.id, "allowlistedBeneficiaries");
         }
