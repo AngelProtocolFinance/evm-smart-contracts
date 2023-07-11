44567372224170be78a0b7d0f19594843f6af4e6
katzman <steve.r.katzman@gmail.com>
2023-07-11 16:26:50 +0000

    Add test for new endpoint on AccountsUPdateStatusEndowments

--- test/core/accounts/AccountsUpdateStatusEndowments.ts
+++ test/core/accounts/AccountsUpdateStatusEndowments.ts
@@ -4,7 +4,7 @@ import {expect, use} from "chai";
 import {BigNumber} from "ethers";
 import hre from "hardhat";
 import { deployFacetAsProxy } from "./utils/deployTestFacet";
-import {DEFAULT_CHARITY_ENDOWMENT, DEFAULT_REGISTRAR_CONFIG} from "test/utils";
+import {DEFAULT_CHARITY_ENDOWMENT, DEFAULT_REGISTRAR_CONFIG, DEFAULT_STRATEGY_SELECTOR} from "test/utils";
 import {
   AccountsUpdateStatusEndowments,
   AccountsUpdateStatusEndowments__factory,
@@ -94,99 +94,118 @@ describe("AccountsUpdateStatusEndowments", function () {
     facet = AccountsUpdateStatusEndowments__factory.connect(state.address, endowOwner);
   });
 
-  it("reverts if the caller is not the owner of the endowment", async () => {
-    await expect(facet.connect(accOwner).closeEndowment(accountId, beneficiary)).to.be.revertedWith(
-      "Unauthorized"
-    );
-  });
+  describe("upon closeEndowmment", async function () {
 
-  it("reverts if the endowment is already closed", async () => {
-    await state.setClosingEndowmentState(accountId, true, beneficiary);
-    await expect(facet.closeEndowment(accountId, beneficiary)).to.be.revertedWith(
-      "Endowment is closed"
-    );
-  });
+    it("reverts if the caller is not the owner of the endowment", async () => {
+      await expect(facet.connect(accOwner).closeEndowment(accountId, beneficiary)).to.be.revertedWith(
+        "Unauthorized"
+      );
+    });
 
-  it("reverts if the beneficiary is set to 'None' and no index fund is configured in the registrar", async () => {
-    registrarFake.queryConfig.returns(DEFAULT_REGISTRAR_CONFIG);
+    it("reverts if the endowment is already closed", async () => {
+      await state.setClosingEndowmentState(accountId, true, beneficiary);
+      await expect(facet.closeEndowment(accountId, beneficiary)).to.be.revertedWith(
+        "Endowment is closed"
+      );
+    });
 
-    const beneficiaryNone: LibAccounts.BeneficiaryStruct = {...beneficiary, enumData: 3};
+    it("reverts if the beneficiary is set to 'None' and no index fund is configured in the registrar", async () => {
+      registrarFake.queryConfig.returns(DEFAULT_REGISTRAR_CONFIG);
 
-    await expect(facet.closeEndowment(accountId, beneficiaryNone)).to.be.revertedWith(
-      "Beneficiary is NONE & Index Fund Contract is not configured in Registrar"
-    );
-  });
+      const beneficiaryNone: LibAccounts.BeneficiaryStruct = {...beneficiary, enumData: 3};
 
-  it("reverts if the endowment has active strategies", async () => {
-    await state.setActiveStrategyEndowmentState(accountId, strategies[0], true);
-    await expect(facet.closeEndowment(accountId, beneficiary)).to.be.revertedWith(
-      "Not fully exited"
-    );
-  });
+      await expect(facet.closeEndowment(accountId, beneficiaryNone)).to.be.revertedWith(
+        "Beneficiary is NONE & Index Fund Contract is not configured in Registrar"
+      );
+    });
 
-  it("removes the closing endowment from all index funds it is involved in", async () => {
-    await expect(facet.closeEndowment(accountId, beneficiary))
-      .to.emit(facet, "EndowmentUpdated")
-      .withArgs(accountId);
+    it("reverts if the endowment has active strategies", async () => {
+      await state.setActiveStrategyEndowmentState(accountId, strategies[0], true);
+      await expect(facet.closeEndowment(accountId, beneficiary)).to.be.revertedWith(
+        "Not fully exited"
+      );
+    });
 
-    const endowState = await state.getClosingEndowmentState(accountId);
+    it("removes the closing endowment from all index funds it is involved in", async () => {
+      await expect(facet.closeEndowment(accountId, beneficiary))
+        .to.emit(facet, "EndowmentUpdated")
+        .withArgs(accountId);
 
-    expect(endowState[0]).to.equal(true);
-    expect(endowState[1].enumData).to.equal(beneficiary.enumData);
-    expect(endowState[1].data.addr).to.equal(beneficiary.data.addr);
-    expect(endowState[1].data.endowId).to.equal(beneficiary.data.endowId);
-    expect(endowState[1].data.fundId).to.equal(beneficiary.data.fundId);
-  });
+      const endowState = await state.getClosingEndowmentState(accountId);
 
-  it("updates the beneficiary to the treasury address if the beneficiary is set to 'None' and the endowment is not involved in any funds", async () => {
-    indexFundFake.queryInvolvedFunds.returns([]);
-    const beneficiaryNone: LibAccounts.BeneficiaryStruct = {...beneficiary, enumData: 3};
+      expect(endowState[0]).to.equal(true);
+      expect(endowState[1].enumData).to.equal(beneficiary.enumData);
+      expect(endowState[1].data.addr).to.equal(beneficiary.data.addr);
+      expect(endowState[1].data.endowId).to.equal(beneficiary.data.endowId);
+      expect(endowState[1].data.fundId).to.equal(beneficiary.data.fundId);
+    });
 
-    await expect(facet.closeEndowment(accountId, beneficiaryNone))
-      .to.emit(facet, "EndowmentUpdated")
-      .withArgs(accountId);
+    it("updates the beneficiary to the treasury address if the beneficiary is set to 'None' and the endowment is not involved in any funds", async () => {
+      indexFundFake.queryInvolvedFunds.returns([]);
+      const beneficiaryNone: LibAccounts.BeneficiaryStruct = {...beneficiary, enumData: 3};
 
-    const endowState = await state.getClosingEndowmentState(accountId);
+      await expect(facet.closeEndowment(accountId, beneficiaryNone))
+        .to.emit(facet, "EndowmentUpdated")
+        .withArgs(accountId);
 
-    expect(endowState[0]).to.equal(true);
-    expect(endowState[1].enumData).to.equal(2);
-    expect(endowState[1].data.addr).to.equal(treasuryAddress);
-    expect(endowState[1].data.endowId).to.equal(0);
-    expect(endowState[1].data.fundId).to.equal(0);
-  });
+      const endowState = await state.getClosingEndowmentState(accountId);
 
-  it("updates the beneficiary to the first index fund if the beneficiary is set to 'None' and the endowment is involved in one or more funds", async () => {
-    const funds: IIndexFund.IndexFundStruct[] = [
-      {
-        description: "d1",
-        expiryTime: BigNumber.from(100),
-        id: BigNumber.from(1),
-        name: "if1",
-        members: [accountId],
-        splitToLiquid: 50,
-      },
-      {
-        description: "d2",
-        expiryTime: BigNumber.from(100),
-        id: BigNumber.from(2),
-        name: "if2",
-        members: [accountId],
-        splitToLiquid: 50,
-      },
-    ];
-    indexFundFake.queryInvolvedFunds.returns(funds);
-    const beneficiaryNone: LibAccounts.BeneficiaryStruct = {...beneficiary, enumData: 3};
+      expect(endowState[0]).to.equal(true);
+      expect(endowState[1].enumData).to.equal(2);
+      expect(endowState[1].data.addr).to.equal(treasuryAddress);
+      expect(endowState[1].data.endowId).to.equal(0);
+      expect(endowState[1].data.fundId).to.equal(0);
+    });
 
-    await expect(facet.closeEndowment(accountId, beneficiaryNone))
-      .to.emit(facet, "EndowmentUpdated")
-      .withArgs(accountId);
+    it("updates the beneficiary to the first index fund if the beneficiary is set to 'None' and the endowment is involved in one or more funds", async () => {
+      const funds: IIndexFund.IndexFundStruct[] = [
+        {
+          description: "d1",
+          expiryTime: BigNumber.from(100),
+          id: BigNumber.from(1),
+          name: "if1",
+          members: [accountId],
+          splitToLiquid: 50,
+        },
+        {
+          description: "d2",
+          expiryTime: BigNumber.from(100),
+          id: BigNumber.from(2),
+          name: "if2",
+          members: [accountId],
+          splitToLiquid: 50,
+        },
+      ];
+      indexFundFake.queryInvolvedFunds.returns(funds);
+      const beneficiaryNone: LibAccounts.BeneficiaryStruct = {...beneficiary, enumData: 3};
 
-    const endowState = await state.getClosingEndowmentState(accountId);
+      await expect(facet.closeEndowment(accountId, beneficiaryNone))
+        .to.emit(facet, "EndowmentUpdated")
+        .withArgs(accountId);
 
-    expect(endowState[0]).to.equal(true);
-    expect(endowState[1].enumData).to.equal(1);
-    expect(endowState[1].data.addr).to.equal(ethers.constants.AddressZero);
-    expect(endowState[1].data.endowId).to.equal(0);
-    expect(endowState[1].data.fundId).to.equal(funds[0].id);
-  });
+      const endowState = await state.getClosingEndowmentState(accountId);
+
+      expect(endowState[0]).to.equal(true);
+      expect(endowState[1].enumData).to.equal(1);
+      expect(endowState[1].data.addr).to.equal(ethers.constants.AddressZero);
+      expect(endowState[1].data.endowId).to.equal(0);
+      expect(endowState[1].data.fundId).to.equal(funds[0].id);
+    });
+  })
+
+  describe("upon forceSetStrategyInactive", async function () {
+
+    it("reverts if the caller is not the endowment owner", async function () {
+      await expect(facet.connect(accOwner).forceSetStrategyInactive(accountId, DEFAULT_STRATEGY_SELECTOR))
+        .to.be.revertedWith("Unauthorized")
+    })
+
+    it("sets the active state to false for the specified strategy", async function () {
+      await state.setActiveStrategyEndowmentState(accountId, DEFAULT_STRATEGY_SELECTOR, true)
+      await facet.connect(endowOwner).forceSetStrategyInactive(accountId, DEFAULT_STRATEGY_SELECTOR)
+      let activeState = await state.getActiveStrategyEndowmentState(accountId, DEFAULT_STRATEGY_SELECTOR)
+      expect(activeState).to.be.false
+    })
+  })
+
 });
