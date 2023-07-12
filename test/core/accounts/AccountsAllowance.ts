5a5b356a4db1bf1ac9b601250565df3fe2ba4ed6
Andrey <SovereignAndrey>
2023-07-12 10:11:27 +0800

    tests passing

--- test/core/accounts/AccountsAllowance.ts
+++ test/core/accounts/AccountsAllowance.ts
@@ -31,33 +31,33 @@ describe("AccountsAllowance", function () {
 
   describe("Test cases for `manageAllowances`", async function () {
     let facet: AccountsAllowance;
-    let proxy: TestFacetProxyContract;
+    let state: TestFacetProxyContract;
 
     beforeEach(async function () {
       let Facet = new AccountsAllowance__factory(owner);
       let facetImpl = await Facet.deploy();
-      proxy = await deployFacetAsProxy(hre, owner, proxyAdmin, facetImpl.address);
-      facet = AccountsAllowance__factory.connect(proxy.address, owner);
+      state = await deployFacetAsProxy(hre, owner, proxyAdmin, facetImpl.address);
+      facet = AccountsAllowance__factory.connect(state.address, owner);
       // set a non-closing endowment up for testing with (#42)
-      await proxy.setClosingEndowmentState(42, false, {
+      await state.setClosingEndowmentState(42, false, {
         data: {endowId: 0, fundId: 0, addr: ethers.constants.AddressZero},
         enumData: 0,
       });
       // add an accepted token to endowment #42
-      await proxy.setTokenAccepted(42, token.address, true);
+      await state.setTokenAccepted(42, token.address, true);
       // set a starting balance for Endowment: 100 qty of tokens in liquid
-      await proxy.setEndowmentTokenBalance(42, token.address, 0, 100);
+      await state.setEndowmentTokenBalance(42, token.address, 0, 100);
       // setup endowment 42 with minimum needed for testing
       // Allowlists Beneficiaries set for a user
       let endowment = DEFAULT_CHARITY_ENDOWMENT;
       endowment.owner = owner.address;
       endowment.allowlistedBeneficiaries = [user.address];
       endowment.maturityAllowlist = [user.address];
-      await proxy.setEndowmentDetails(42, endowment);
+      await state.setEndowmentDetails(42, endowment);
     });
 
     it("reverts when the endowment is closed", async function () {
-      await proxy.setClosingEndowmentState(42, true, {
+      await state.setClosingEndowmentState(42, true, {
         data: {endowId: 0, fundId: 0, addr: ethers.constants.AddressZero},
         enumData: 0,
       });
@@ -91,7 +91,7 @@ describe("AccountsAllowance", function () {
 
     it("reverts when there are no adjustments needed (ie. proposed amount == spender balance amount)", async function () {
       // set allowance for user to 10 tokens of total 10 tokens outstanding
-      await proxy.setTokenAllowance(42, user.address, token.address, 10, 10);
+      await state.setTokenAllowance(42, user.address, token.address, 10, 10);
 
       await expect(facet.manageAllowances(42, user.address, token.address, 10)).to.be.revertedWith(
         "Spender balance equal to amount. No changes needed"
@@ -105,35 +105,37 @@ describe("AccountsAllowance", function () {
     });
 
     it("passes when try to increase a valid token's allowance within range of liquid balance available", async function () {
-      expect(await facet.manageAllowances(42, user.address, token.address, 10));
+      expect(await facet.manageAllowances(42, user.address, token.address, 10))
+        .to.emit(facet, "AllowanceUpdated")
+        .withArgs(42, user.address, token.address, 10, 10, 0);
 
       // endowment liquid balance should be 90 now (100 - 10)
-      const endowBal = await proxy.getEndowmentTokenBalance(42, token.address);
+      const endowBal = await state.getEndowmentTokenBalance(42, token.address);
       expect(endowBal[1]).to.equal(90);
       // user allowance should be 10 now
-      const allowance = await proxy.getTokenAllowance(42, user.address, token.address);
+      const allowance = await state.getTokenAllowance(42, user.address, token.address);
       expect(allowance).to.equal(10);
     });
   });
 
   describe("Test cases for `spendAllowance`", async function () {
     let facet: AccountsAllowance;
-    let proxy: TestFacetProxyContract;
+    let state: TestFacetProxyContract;
 
     beforeEach(async function () {
       let Facet = new AccountsAllowance__factory(owner);
       let facetImpl = await Facet.deploy();
-      proxy = await deployFacetAsProxy(hre, owner, proxyAdmin, facetImpl.address);
-      facet = AccountsAllowance__factory.connect(proxy.address, owner);
+      state = await deployFacetAsProxy(hre, owner, proxyAdmin, facetImpl.address);
+      facet = AccountsAllowance__factory.connect(state.address, owner);
       // set a non-closing endowment up for testing with (#42)
-      await proxy.setClosingEndowmentState(42, false, {
+      await state.setClosingEndowmentState(42, false, {
         data: {endowId: 0, fundId: 0, addr: ethers.constants.AddressZero},
         enumData: 0,
       });
       // add an accepted token to endowment #42
-      await proxy.setTokenAccepted(42, token.address, true);
+      await state.setTokenAccepted(42, token.address, true);
       // set a starting balance for Endowment: 100 qty of tokens in liquid
-      await proxy.setEndowmentTokenBalance(42, token.address, 0, 100);
+      await state.setEndowmentTokenBalance(42, token.address, 0, 100);
     });
 
     it("reverts when try to spend token that is invalid(zero address) or dne in allowances", async function () {
@@ -150,7 +152,7 @@ describe("AccountsAllowance", function () {
 
     it("reverts when try to spend allowance to an invalid recipient address", async function () {
       // now we allocate some token allowance to the user address to spend from
-      await proxy.setTokenAllowance(42, user.address, token.address, 10, 10);
+      await state.setTokenAllowance(42, user.address, token.address, 10, 10);
 
       // try to spend allocated funds to an invalid zero address
       await expect(
@@ -160,7 +162,7 @@ describe("AccountsAllowance", function () {
 
     it("reverts when try to spend zero amount of allowance", async function () {
       // now we allocate some token allowance to the user address to spend from
-      await proxy.setTokenAllowance(42, user.address, token.address, 10, 10);
+      await state.setTokenAllowance(42, user.address, token.address, 10, 10);
 
       // try to spend zero allowance
       await expect(facet.spendAllowance(42, token.address, 0, user.address)).to.be.revertedWith(
@@ -170,7 +172,7 @@ describe("AccountsAllowance", function () {
 
     it("reverts when try to spend more allowance than is available for token", async function () {
       // now we allocate some token allowance to the user address to spend from
-      await proxy.setTokenAllowance(42, user.address, token.address, 10, 10);
+      await state.setTokenAllowance(42, user.address, token.address, 10, 10);
 
       // try to spend more allowance than user was allocated
       await expect(facet.spendAllowance(42, token.address, 1000, user.address)).to.be.revertedWith(
@@ -180,16 +182,18 @@ describe("AccountsAllowance", function () {
 
     it("passes when spend less than or equal to the allowance available for token", async function () {
       // now we allocate some token allowance to the user address to spend from
-      await proxy.setTokenAllowance(42, user.address, token.address, 10, 10);
+      await state.setTokenAllowance(42, user.address, token.address, 10, 10);
 
       // mint tokens so that the contract can transfer them to recipient
       await token.mint(facet.address, 10);
 
       // user spends less than what was allocated to them (ie. 5 out of 10 available)
-      expect(await facet.connect(user).spendAllowance(42, token.address, 5, user.address));
+      expect(await facet.connect(user).spendAllowance(42, token.address, 5, user.address))
+        .to.emit(facet, "AllowanceSpent")
+        .withArgs(42, user.address, token.address, 5);
 
       // user allowance should be 5 now (10 - 5)
-      let allowance = await proxy.getTokenAllowance(42, user.address, token.address);
+      let allowance = await state.getTokenAllowance(42, user.address, token.address);
       expect(allowance).to.equal(5);
     });
   });
