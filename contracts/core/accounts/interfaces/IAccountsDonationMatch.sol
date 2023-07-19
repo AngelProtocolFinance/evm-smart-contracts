968cc6bfd9cb155debeeecd2fd096e623c59fbb8
Nenad <n.misic@outlook.com>
2023-07-19 08:05:08 +0200

    Add missing functions to IAccountsDonationMatch

--- contracts/core/accounts/interfaces/IAccountsDonationMatch.sol
+++ contracts/core/accounts/interfaces/IAccountsDonationMatch.sol
@@ -1,6 +1,12 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.16;
 
+import {AccountMessages} from "../message.sol";
+
 interface IAccountsDonationMatch {
-  function depositDonationMatchErC20(uint32 id, address token, uint256 amount) external;
+  function depositDonationMatchERC20(uint32 id, address token, uint256 amount) external;
+
+  function withdrawDonationMatchERC20(uint32 id, address recipient, uint256 amount) external;
+
+  function setupDonationMatch(uint32 id, AccountMessages.DonationMatch memory details) external;
 }
