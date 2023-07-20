45d1f8299215515c84c0fb0fd270b15f2c387d32
Nenad <n.misic@outlook.com>
2023-07-20 11:01:55 +0200

    Remove msAddress

--- contracts/multisigs/interfaces/ICharityApplications.sol
+++ contracts/multisigs/interfaces/ICharityApplications.sol
@@ -8,12 +8,7 @@ abstract contract ICharityApplications {
   /*
    * Events
    */
-  event ApplicationProposed(
-    address msAddress,
-    uint256 proposalId,
-    address proposer,
-    string charityName
-  );
+  event ApplicationProposed(uint256 proposalId, address proposer, string charityName);
   event ApplicationExecuted(uint256 proposalId);
   event ApplicationConfirmed(uint256 proposalId, address owner);
   event ApplicationConfirmationRevoked(uint256 proposalId, address owner);
