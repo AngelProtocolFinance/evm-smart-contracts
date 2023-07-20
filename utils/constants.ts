54dc4857a69037cdb91bd0cae08da143ea78ebf5
Nenad <n.misic@outlook.com>
2023-07-20 09:02:02 +0200

    Fix updateNetworkConnections script to use enum NetworkConnectionAction.POST

--- utils/constants.ts
+++ utils/constants.ts
@@ -3,3 +3,9 @@ import path from "path";
 export const ADDRESS_ZERO = "0x0000000000000000000000000000000000000000";
 
 export const DEFAULT_CONTRACT_ADDRESS_FILE_PATH = path.join(__dirname, "../contract-address.json");
+
+export enum NetworkConnectionAction {
+  NONE,
+  POST,
+  DELETE,
+}
