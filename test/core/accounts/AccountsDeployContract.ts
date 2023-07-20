c9f7dd81b77cacf58c648c0e7c76164aa76906d7
Nenad <n.misic@outlook.com>
2023-07-20 14:04:41 +0200

    Add passing test

--- test/core/accounts/AccountsDeployContract.ts
+++ test/core/accounts/AccountsDeployContract.ts
@@ -1,4 +1,5 @@
 import {FakeContract, smock} from "@defi-wonderland/smock";
+import {impersonateAccount, setBalance} from "@nomicfoundation/hardhat-network-helpers";
 import {SignerWithAddress} from "@nomiclabs/hardhat-ethers/signers";
 import {expect, use} from "chai";
 import hre from "hardhat";
@@ -14,6 +15,7 @@ import {
   SubDao__factory,
   TestFacetProxyContract,
 } from "typechain-types";
+import {SubDaoMessages} from "typechain-types/contracts/core/accounts/facets/AccountsDeployContract";
 import {RegistrarStorage} from "typechain-types/contracts/core/registrar/Registrar";
 import {genWallet, getSigners} from "utils";
 import {deployFacetAsProxy} from "./utils/deployTestFacet";
@@ -27,7 +29,7 @@ describe("AccountsDeployContract", function () {
 
   let accOwner: SignerWithAddress;
   let proxyAdmin: SignerWithAddress;
-  let endowOwner: SignerWithAddress;
+  let user: SignerWithAddress;
 
   let facet: AccountsDeployContract;
   let state: TestFacetProxyContract;
@@ -40,7 +42,7 @@ describe("AccountsDeployContract", function () {
     const signers = await getSigners(hre);
     accOwner = signers.apTeam1;
     proxyAdmin = signers.proxyAdmin;
-    endowOwner = signers.deployer;
+    user = signers.deployer;
 
     subdaoFake = await smock.fake<SubDao>(
       new SubDao__factory({
@@ -80,47 +82,72 @@ describe("AccountsDeployContract", function () {
   });
 
   describe("upon createDaoContract", () => {
-    it("reverts if caller is not Accounts Diamond itself", async () => {
-      await expect(
-        facet.connect(endowOwner).createDaoContract({
-          endowOwner: endowOwner.address,
-          endowType: EndowmentType.Charity,
-          expirationPeriod: 1,
-          id: endowId,
-          owner: endowOwner.address,
-          proposalDeposit: 1,
-          quorum: 1,
-          registrarContract: registrarFake.address,
-          snapshotPeriod: 1,
-          threshold: 1,
-          timelockPeriod: 1,
-          token: {
-            token: TokenType.New,
-            data: {
-              existingData: genWallet().address,
-              newInitialSupply: 1,
-              newName: "newname",
-              newSymbol: "NEW",
-              veBondingDecimals: 18,
-              veBondingName: "VENAME",
-              veBondingPeriod: 1,
-              veBondingReserveDecimals: 18,
-              veBondingReserveDenom: genWallet().address,
-              veBondingSymbol: "DSFN",
-              veBondingType: {
-                ve_type: VeTypeEnum.Constant,
-                data: {
-                  power: 1,
-                  scale: 1,
-                  slope: 1,
-                  value: 1,
-                },
+    let createDaoMessage: SubDaoMessages.InstantiateMsgStruct;
+
+    before(() => {
+      const endowOwner = genWallet().address;
+
+      createDaoMessage = {
+        endowOwner: endowOwner,
+        endowType: EndowmentType.Charity,
+        expirationPeriod: 1,
+        id: endowId,
+        owner: endowOwner,
+        proposalDeposit: 1,
+        quorum: 1,
+        registrarContract: registrarFake.address,
+        snapshotPeriod: 1,
+        threshold: 1,
+        timelockPeriod: 1,
+        token: {
+          token: TokenType.New,
+          data: {
+            existingData: genWallet().address,
+            newInitialSupply: 1,
+            newName: "newname",
+            newSymbol: "NEW",
+            veBondingDecimals: 18,
+            veBondingName: "VENAME",
+            veBondingPeriod: 1,
+            veBondingReserveDecimals: 18,
+            veBondingReserveDenom: genWallet().address,
+            veBondingSymbol: "DSFN",
+            veBondingType: {
+              ve_type: VeTypeEnum.Constant,
+              data: {
+                power: 1,
+                scale: 1,
+                slope: 1,
+                value: 1,
               },
             },
           },
-          votingPeriod: 1,
-        })
-      ).to.be.revertedWith("Unauthorized");
+        },
+        votingPeriod: 1,
+      };
+    });
+
+    it("reverts if caller is not Accounts Diamond itself", async () => {
+      await expect(facet.connect(user).createDaoContract(createDaoMessage)).to.be.revertedWith(
+        "Unauthorized"
+      );
+    });
+
+    it("passes when called by Accounts Diamond itself", async () => {
+      await impersonateAccount(state.address);
+      await setBalance(state.address, ethers.utils.parseEther("1"));
+
+      const accDiamond = await ethers.getSigner(state.address);
+
+      // no need to mock SubDaoEmitter.initializeSubDao & SubDao.buildDaoTokenMesage
+      // as their "fake" versions do nothing by default
+
+      expect(await facet.connect(accDiamond).createDaoContract(createDaoMessage))
+        .to.not.equal(ethers.constants.AddressZero)
+        .and.to.emit(facet, "DaoContractCreated");
+
+      expect(subdaoEmitterFake.initializeSubDao).to.have.been.called;
+      expect(subdaoFake.buildDaoTokenMesage).to.have.been.called;
     });
   });
 });
