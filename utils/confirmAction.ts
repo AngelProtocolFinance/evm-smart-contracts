7a335bc22c510a5368ed017df1e8f7c64bb9f9c6
Nenad <n.misic@outlook.com>
2023-07-27 09:43:01 +0200

    Make confirmAction's param optional

--- utils/confirmAction.ts
+++ utils/confirmAction.ts
@@ -1,8 +1,10 @@
 import {question} from "readline-sync";
 
-export async function confirmAction(actionDescription: string): Promise<boolean> {
+export async function confirmAction(actionDescription?: string): Promise<boolean> {
   const answer = question(
-    `${actionDescription}\nAre you sure you wish to perform this action? (y/N)`
+    `${
+      actionDescription ? `${actionDescription}\n` : ""
+    }Are you sure you wish to perform this action? (y/N)`
   ).trim();
   return /^(y|yes)$/i.test(answer);
 }
