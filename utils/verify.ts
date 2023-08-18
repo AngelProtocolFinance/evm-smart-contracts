5ada86aad5ed0c9076350ca05edd8ea9ca8b20a2
katzman <steve.r.katzman@gmail.com>
2023-08-18 23:18:55 +0000

    Added strict checking of contractName to Deployment arg

--- utils/verify.ts
+++ utils/verify.ts
@@ -1,11 +1,14 @@
 import {HardhatRuntimeEnvironment} from "hardhat/types";
-import {Deployment, logger} from ".";
+import {Deployment, logger, getContractName} from ".";
 
 export async function verify(hre: HardhatRuntimeEnvironment, deployment: Deployment) {
   try {
     logger.out(`Verifying ${deployment.contractName ?? "contract"} at: ${deployment.address}...`);
+    if(!deployment.contractName) {
+      throw new Error("Contract name required")
+    }
     await hre.tenderly.verify({
-      name: deployment.contractName!,
+      name: deployment.contractName,
       address: deployment.address,
     });
     await hre.run("verify:verify", deployment);
