78c15d681b9abb34de57b2ffb4ed9002d39830c6
Nenad <n.misic@outlook.com>
2023-08-24 12:54:41 +0200

    Remove 'Ownable' import from IGasFwdFactory

--- contracts/core/gasFwd/IGasFwdFactory.sol
+++ contracts/core/gasFwd/IGasFwdFactory.sol
@@ -2,7 +2,6 @@
 pragma solidity ^0.8.19;
 
 import {ProxyContract} from "../proxy.sol";
-import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
 
 interface IGasFwdFactory {
   error InvalidAddress(string param);
