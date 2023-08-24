ab72ba284d972f91f689854f76a78da8a57b7c44
Nenad <n.misic@outlook.com>
2023-08-24 13:17:08 +0200

    Create helper 'getEvents' and use in addMultisigOwner

--- tasks/manage/addMultisigOwner.ts
+++ tasks/manage/addMultisigOwner.ts
@@ -1,6 +1,6 @@
 import {task} from "hardhat/config";
 import {MultiSigGeneric__factory} from "typechain-types";
-import {getSigners, logger} from "utils";
+import {getEvents, getSigners, logger} from "utils";
 
 type TaskArgs = {multisig: string; owner: string};
 
@@ -36,23 +36,16 @@ task("manage:addMultisigOwner", "Will add the specified address to the multisig 
       logger.out(`Tx hash: ${tx.hash}`);
       const receipt = await tx.wait();
 
-      const eventName = "TransactionSubmitted";
-      if (!receipt.events?.length) {
-        throw new Error("Unexpected: no events emitted.");
-      }
-      const transactionSubmittedEvent = receipt.events
-        .filter((event) => event.event === eventName)
-        ?.at(0);
+      const transactionSubmittedEvent = getEvents(
+        receipt.events,
+        multisig,
+        multisig.filters.TransactionSubmitted()
+      ).at(0);
       if (!transactionSubmittedEvent) {
-        throw new Error(`Unexpected: ${eventName} not emitted.`);
+        throw new Error("Unexpected: TransactionSubmitted not emitted.");
       }
-      if (!transactionSubmittedEvent.args) {
-        throw new Error(`Unexpected: no args in ${eventName} event.`);
-      }
-      const transactionId = transactionSubmittedEvent.args["transactionId"];
-      if (!transactionId) {
-        throw new Error(`Unexpected: no transactionId in ${eventName} args.`);
-      }
+
+      const transactionId = transactionSubmittedEvent.args.transactionId;
 
       if ((await multisig.transactions(transactionId)).executed) {
         if (await multisig.isOwner(taskArguments.owner)) {
