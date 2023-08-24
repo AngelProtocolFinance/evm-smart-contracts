9d74a5348a9af19527a0b9f433d24ad3bbbc44c1
Nenad <n.misic@outlook.com>
2023-08-24 11:51:22 +0200

    Improve logic

--- tasks/manage/addMultisigOwner.ts
+++ tasks/manage/addMultisigOwner.ts
@@ -34,12 +34,38 @@ task("manage:addMultisigOwner", "Will add the specified address to the multisig 
         "0x"
       );
       logger.out(`Tx hash: ${tx.hash}`);
-      await tx.wait();
+      const receipt = await tx.wait();
 
-      if (await multisig.isOwner(taskArguments.owner)) {
-        logger.out(`Account with address ${taskArguments.owner} is now one of the owners.`);
+      const eventName = "TransactionSubmitted";
+      if (!receipt.events?.length) {
+        throw new Error("Unexpected: no events emitted.");
+      }
+      const transactionSubmittedEvent = receipt.events
+        .filter((event) => event.event === eventName)
+        ?.at(0);
+      if (!transactionSubmittedEvent) {
+        throw new Error(`Unexpected: ${eventName} not emitted.`);
+      }
+      if (!transactionSubmittedEvent.args) {
+        throw new Error(`Unexpected: no args in ${eventName} event.`);
+      }
+      const transactionId = transactionSubmittedEvent.args["transactionId"];
+      if (!transactionId) {
+        throw new Error(`Unexpected: no transactionId in ${eventName} args.`);
+      }
+
+      if ((await multisig.transactions(transactionId)).executed) {
+        if (await multisig.isOwner(taskArguments.owner)) {
+          logger.out(`Account with address ${taskArguments.owner} is now one of the owners.`);
+        } else {
+          throw new Error("Unexpected: adding new owner failed");
+        }
+      } else if (await multisig.isConfirmed(transactionId)) {
+        logger.out(`Transaction with ID "${transactionId}" confirmed, awaiting execution.`);
       } else {
-        throw new Error("Adding new owner failed");
+        logger.out(
+          `Transaction with ID "${transactionId}" submitted, awaiting confirmation by other owners.`
+        );
       }
     } catch (error) {
       logger.out(error, logger.Level.Error);
