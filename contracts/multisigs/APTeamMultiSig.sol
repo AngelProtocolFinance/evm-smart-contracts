4f0c1e246c7bbc8e3b66a5b06d9cf032374f91ac
Nenad <n.misic@outlook.com>
2023-08-25 11:54:29 +0200

    Add comments to APTeamMultiSig.initializeAPTeam

--- contracts/multisigs/APTeamMultiSig.sol
+++ contracts/multisigs/APTeamMultiSig.sol
@@ -4,6 +4,11 @@ pragma solidity ^0.8.19;
 import {MultiSigGeneric} from "./MultiSigGeneric.sol";
 
 contract APTeamMultiSig is MultiSigGeneric {
+  /// @dev Initializes MultiSig's state variables.
+  /// @param owners List of initial owners.
+  /// @param _approvalsRequired Number of required confirmations.
+  /// @param _requireExecution setting for if an explicit execution call is required
+  /// @param _transactionExpiry Proposal expiry time in seconds
   function initializeAPTeam(
     address[] memory owners,
     uint256 _approvalsRequired,
