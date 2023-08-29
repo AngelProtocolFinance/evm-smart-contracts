f34acd42ab492ec9b4c34491bdc77387f559a186
Nenad <n.misic@outlook.com>
2023-08-29 13:14:44 +0200

    Remove address validation in OwnershipFacet.owner

--- contracts/core/accounts/diamond/facets/OwnershipFacet.sol
+++ contracts/core/accounts/diamond/facets/OwnershipFacet.sol
@@ -3,7 +3,6 @@ pragma solidity ^0.8.19;
 
 import {LibDiamond} from "../libraries/LibDiamond.sol";
 import {IERC173} from "../interfaces/IERC173.sol";
-import {Validator} from "../../../validator.sol";
 
 contract OwnershipFacet is IERC173 {
   function transferOwnership(address newOwner) external override {
@@ -12,7 +11,6 @@ contract OwnershipFacet is IERC173 {
   }
 
   function owner() external view override returns (address owner_) {
-    require(Validator.addressChecker(owner_), "Invalid address");
     owner_ = LibDiamond.contractOwner();
   }
 }
