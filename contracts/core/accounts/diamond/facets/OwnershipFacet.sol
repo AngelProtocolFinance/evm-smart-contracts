a84d96da90c7e0bd94bc968c57c7b74b4db82d2a
Nenad <n.misic@outlook.com>
2023-08-29 16:12:51 +0200

    Add missing address validation to OwnershipFacet.transferOwnership

--- contracts/core/accounts/diamond/facets/OwnershipFacet.sol
+++ contracts/core/accounts/diamond/facets/OwnershipFacet.sol
@@ -3,10 +3,12 @@ pragma solidity ^0.8.19;
 
 import {LibDiamond} from "../libraries/LibDiamond.sol";
 import {IERC173} from "../interfaces/IERC173.sol";
+import {Validator} from "../../../validator.sol";
 
 contract OwnershipFacet is IERC173 {
   function transferOwnership(address newOwner) external override {
     LibDiamond.enforceIsContractOwner();
+    require(Validator.addressChecker(newOwner), "Invalid address");
     LibDiamond.setContractOwner(newOwner);
   }
 
