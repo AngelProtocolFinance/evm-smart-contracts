1cfef56b3c55460720ab92caaadc67d006a4012c
Nenad <n.misic@outlook.com>
2023-08-31 12:16:47 +0200

    Fix bug with hh compile: remove ownership check from get[APTeam/ProxyAdmin]Owner funcs

--- utils/signers/getAPTeamOwner.ts
+++ utils/signers/getAPTeamOwner.ts
@@ -1,8 +1,5 @@
 import {SignerWithAddress} from "@nomiclabs/hardhat-ethers/signers";
 import {HardhatRuntimeEnvironment} from "hardhat/types";
-// import {APTeamMultiSig__factory} from "typechain-types";
-import {getContractName} from "../getContractName";
-import {getAddresses} from "../manageAddressFile";
 import {connectSignerFromPkey} from "./connectSignerFromPkey";
 import {getSigners} from "./getSigners";
 
@@ -10,27 +7,14 @@ export async function getAPTeamOwner(
   hre: HardhatRuntimeEnvironment,
   pkey?: string
 ): Promise<SignerWithAddress> {
-  // const msName = getContractName(new APTeamMultiSig__factory());
-  const error = new Error(`Must provide a pkey for ${"msName"} owner on this network`);
-  if (!pkey) {
-    const {apTeamMultisigOwners} = await getSigners(hre);
-    if (!apTeamMultisigOwners || apTeamMultisigOwners.length === 0) {
-      throw error;
-    }
-    return apTeamMultisigOwners[0];
+  if (pkey) {
+    return await connectSignerFromPkey(pkey, hre);
   }
 
-  const signer = await connectSignerFromPkey(pkey, hre);
-  const addresses = await getAddresses(hre);
+  const {apTeamMultisigOwners} = await getSigners(hre);
 
-  // const apTeamMultiSig = APTeamMultiSig__factory.connect(
-  //   addresses.multiSig.apTeam.proxy,
-  //   hre.ethers.provider
-  // );
-  // const isOwner = await apTeamMultiSig.isOwner(signer.address);
-  // if (!isOwner) {
-  //   throw error;
-  // }
-
-  return signer;
+  if (!apTeamMultisigOwners || apTeamMultisigOwners.length === 0) {
+    throw new Error("Must provide a pkey for APTeamMultiSig owner on this network");
+  }
+  return apTeamMultisigOwners[0];
 }
--- utils/signers/getProxyAdminOwner.ts
+++ utils/signers/getProxyAdminOwner.ts
@@ -1,8 +1,5 @@
 import {SignerWithAddress} from "@nomiclabs/hardhat-ethers/signers";
 import {HardhatRuntimeEnvironment} from "hardhat/types";
-import {ProxyAdminMultiSig__factory} from "typechain-types";
-import {getContractName} from "../getContractName";
-import {getAddresses} from "../manageAddressFile";
 import {connectSignerFromPkey} from "./connectSignerFromPkey";
 import {getSigners} from "./getSigners";
 
@@ -10,28 +7,14 @@ export async function getProxyAdminOwner(
   hre: HardhatRuntimeEnvironment,
   pkey?: string
 ): Promise<SignerWithAddress> {
-  const msName = getContractName(new ProxyAdminMultiSig__factory());
-  const error = new Error(`Must provide a pkey for ${msName} owner on this network`);
-
-  if (!pkey) {
-    const {proxyAdminMultisigOwners} = await getSigners(hre);
-    if (!proxyAdminMultisigOwners || proxyAdminMultisigOwners.length === 0) {
-      throw error;
-    }
-    return proxyAdminMultisigOwners[0];
+  if (pkey) {
+    return await connectSignerFromPkey(pkey, hre);
   }
 
-  const signer = await connectSignerFromPkey(pkey, hre);
-  const addresses = await getAddresses(hre);
+  const {proxyAdminMultisigOwners} = await getSigners(hre);
 
-  const proxyAdminMultiSig = ProxyAdminMultiSig__factory.connect(
-    addresses.multiSig.proxyAdmin,
-    hre.ethers.provider
-  );
-  const isOwner = await proxyAdminMultiSig.isOwner(signer.address);
-  if (!isOwner) {
-    throw error;
+  if (!proxyAdminMultisigOwners || proxyAdminMultisigOwners.length === 0) {
+    throw new Error("Must provide a pkey for ProxyAdminMultiSig owner on this network");
   }
-
-  return signer;
+  return proxyAdminMultisigOwners[0];
 }
