b459145a4fbf2a40462cee81044b37d220a93bcc
Nenad <n.misic@outlook.com>
2023-09-01 09:28:44 +0200

    Extract changeAdmin > submitMultiSigTx into a separate helper file

---
+++ tasks/helpers/submitMultiSigTx.ts
@@ -0,0 +1,68 @@
+import {SignerWithAddress} from "@nomiclabs/hardhat-ethers/signers";
+import {BytesLike} from "ethers";
+import {IMultiSigGeneric__factory} from "typechain-types";
+import {getEvents, logger} from "utils";
+
+/**
+ * Submits a transaction to the designated Multisig contract and executes it if possible.
+ * @param msAddress address of the Multisig contract
+ * @param msOwner signer representing an owner of the Multisig with the ability to submit, confirm and execute transactions
+ * @param destination transaction target address
+ * @param data transaction data payload
+ * @returns boolean value indicating whether the transaction was executed or not (i.e. is pending confirmation by other owners)
+ */
+export async function submitMultiSigTx(
+  msAddress: string,
+  msOwner: SignerWithAddress,
+  destination: string,
+  data: BytesLike
+): Promise<boolean> {
+  logger.out(`Submitting transaction to Multisig at address: ${msAddress}...`);
+  const multisig = IMultiSigGeneric__factory.connect(msAddress, msOwner);
+  const tx = await multisig.submitTransaction(destination, 0, data, "0x");
+  logger.out(`Tx hash: ${tx.hash}`);
+  const receipt = await tx.wait();
+
+  const transactionExecutedEvent = getEvents(
+    receipt.events,
+    multisig,
+    multisig.filters.TransactionExecuted()
+  ).at(0);
+  if (transactionExecutedEvent) {
+    return true;
+  }
+
+  const transactionSubmittedEvent = getEvents(
+    receipt.events,
+    multisig,
+    multisig.filters.TransactionSubmitted()
+  ).at(0);
+  if (!transactionSubmittedEvent) {
+    throw new Error(`Unexpected: ${multisig.filters.TransactionSubmitted.name} not emitted.`);
+  }
+
+  const txId = transactionSubmittedEvent.args.transactionId;
+
+  const isConfirmed = await multisig.isConfirmed(txId);
+  if (!isConfirmed) {
+    logger.out(`Transaction with ID "${txId}" submitted, awaiting confirmation by other owners.`);
+    return false;
+  }
+
+  // is confirmed but not executed -> requires manual execution
+  logger.out(`Executing the new charity endowment with transaction ID: ${txId}...`);
+  const tx2 = await multisig.executeTransaction(txId);
+  logger.out(`Tx hash: ${tx2.hash}`);
+  const execReceipt = await tx2.wait();
+
+  const txExecuted = getEvents(
+    execReceipt.events,
+    multisig,
+    multisig.filters.TransactionExecuted()
+  ).at(0);
+  if (!txExecuted) {
+    throw new Error(`Unexpected: ${multisig.filters.TransactionExecuted.name} not emitted.`);
+  }
+
+  return true;
+}
--- tasks/manage/changeAdmin.ts
+++ tasks/manage/changeAdmin.ts
@@ -1,21 +1,13 @@
 import {SignerWithAddress} from "@nomiclabs/hardhat-ethers/signers";
-import {BytesLike} from "ethers";
 import {task} from "hardhat/config";
 import {HardhatRuntimeEnvironment} from "hardhat/types";
 import {
-  IMultiSigGeneric__factory,
   ITransparentUpgradeableProxy__factory,
   OwnershipFacet__factory,
   ProxyContract__factory,
 } from "typechain-types";
-import {
-  AddressObj,
-  confirmAction,
-  getAddresses,
-  getEvents,
-  getProxyAdminOwner,
-  logger,
-} from "utils";
+import {AddressObj, confirmAction, getAddresses, getProxyAdminOwner, logger} from "utils";
+import {submitMultiSigTx} from "../helpers";
 
 type TaskArgs = {
   apTeamSignerPkey?: string;
@@ -155,60 +147,4 @@ function extractProxyContractAddresses(key: string, value: any): {name: string; 
   }
 
   return Object.entries(value).flatMap(([key, val]) => extractProxyContractAddresses(key, val));
-}
-
-async function submitMultiSigTx(
-  msAddress: string,
-  proxyAdminOwner: SignerWithAddress,
-  destination: string,
-  data: BytesLike
-): Promise<boolean> {
-  logger.out(`Submitting transaction to Multisig at address: ${msAddress}...`);
-  const multisig = IMultiSigGeneric__factory.connect(msAddress, proxyAdminOwner);
-  const tx = await multisig.submitTransaction(destination, 0, data, "0x");
-  logger.out(`Tx hash: ${tx.hash}`);
-  const receipt = await tx.wait();
-
-  const transactionExecutedEvent = getEvents(
-    receipt.events,
-    multisig,
-    multisig.filters.TransactionExecuted()
-  ).at(0);
-  if (transactionExecutedEvent) {
-    return true;
-  }
-
-  const transactionSubmittedEvent = getEvents(
-    receipt.events,
-    multisig,
-    multisig.filters.TransactionSubmitted()
-  ).at(0);
-  if (!transactionSubmittedEvent) {
-    throw new Error("Unexpected: TransactionSubmitted not emitted.");
-  }
-
-  const txId = transactionSubmittedEvent.args.transactionId;
-
-  const isConfirmed = await multisig.isConfirmed(txId);
-  if (!isConfirmed) {
-    logger.out(`Transaction with ID "${txId}" submitted, awaiting confirmation by other owners.`);
-    return false;
-  }
-
-  // is confirmed but not executed -> requires manual execution
-  logger.out(`Executing the new charity endowment with transaction ID: ${txId}...`);
-  const tx2 = await multisig.executeTransaction(txId);
-  logger.out(`Tx hash: ${tx2.hash}`);
-  const execReceipt = await tx2.wait();
-
-  const txExecuted = getEvents(
-    execReceipt.events,
-    multisig,
-    multisig.filters.TransactionExecuted()
-  ).at(0);
-  if (!txExecuted) {
-    throw new Error("Unexpected: TransactionExecuted not emitted.");
-  }
-
-  return true;
 }
