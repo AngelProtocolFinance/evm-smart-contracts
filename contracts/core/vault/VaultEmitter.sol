dc4ccd5f3d1bbe5bfc35291f2acade492b8ecb72
Nenad <n.misic@outlook.com>
2023-09-05 14:24:45 +0200

    Update contract pragma to ^0.8.19

--- contracts/core/vault/VaultEmitter.sol
+++ contracts/core/vault/VaultEmitter.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.16;
+pragma solidity ^0.8.19;
 
 import {IVault} from "./interfaces/IVault.sol";
 import {IVaultEmitter} from "./interfaces/IVaultEmitter.sol";
