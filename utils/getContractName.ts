0a9ed5b69388ca9ed3daca24e4bd339062c5870d
Nenad <n.misic@outlook.com>
2023-09-06 13:14:21 +0200

    Revert change to utils/getContractName.ts

--- utils/getContractName.ts
+++ utils/getContractName.ts
@@ -1,8 +1,5 @@
 import {ContractFactory} from "ethers";
 
-export function getContractName<T extends ContractFactory>(
-  factory: T | {new (...args: any): T}
-): string {
-  const factoryName = "name" in factory ? factory.name : factory.constructor.name;
-  return factoryName.replace("__factory", "");
+export function getContractName<T extends ContractFactory>(factory: T): string {
+  return factory.constructor.name.replace("__factory", "");
 }
