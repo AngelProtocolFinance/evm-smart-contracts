a87ade4680d7c31fc81e35687388d23dd6ed8aaf
Nenad <n.misic@outlook.com>
2023-09-06 12:29:36 +0200

    Refactor deploy

--- utils/deploy.ts
+++ utils/deploy.ts
@@ -2,18 +2,20 @@ import {SignerWithAddress} from "@nomiclabs/hardhat-ethers/signers";
 import {ContractFactory} from "ethers";
 import {getContractName, logger} from ".";
 
+type Deployment<T extends ContractFactory> = {
+  constructorArguments?: Parameters<T["deploy"]>;
+  contract: Awaited<ReturnType<T["deploy"]>>;
+  contractName: string;
+};
+
 export async function deploy<T extends ContractFactory>(
   Type: {new (...args: any): T},
   deployer: SignerWithAddress,
   constructorArguments?: Parameters<T["deploy"]>
-): Promise<{
-  contract: Awaited<ReturnType<T["deploy"]>>;
-  constructorArguments?: Parameters<T["deploy"]>;
-  contractName: string;
-}> {
-  const Contract = new Type(deployer);
-  const contractName = getContractName(Contract);
+): Promise<Deployment<T>> {
+  const contractName = getContractName(Type);
   logger.out(`Deploying ${contractName}...`);
+  const Contract = new Type(deployer);
   const contract = await Contract.deploy(...(constructorArguments ?? []));
   try {
     await contract.deployed();
