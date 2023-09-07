c9bfe25494117a4b79e93150f258fe7b22a1c905
Nenad Misic <nenad@angelprotocol.io>
2023-09-07 10:35:04 +0200

    AP-721: Minor fix to `manage:registrar:setAllFeeSettings` (#369)
    * Extract getKeysTyped function
    
    * Fix manage:registrar:setAllFeeSettings
    
    * Revert contract-address.json changes

--- utils/structToObject.ts
+++ utils/structToObject.ts
@@ -1,8 +1,10 @@
+import {getKeysTyped} from "./getKeysTyped";
+
 export function structToObject<T>(struct: T): T {
   if (!struct || typeof struct !== "object") {
     return struct;
   }
-  return (Object.keys(struct) as (keyof T)[])
+  return getKeysTyped(struct)
     .filter((key) => isNaN(Number(key)))
     .reduce((res, key) => {
       res[key] = structToObject(struct[key]);
