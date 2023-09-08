75374910bce65caac96188a755c342ca6f908d8f
katzman <steve.r.katzman@gmail.com>
2023-09-08 04:10:54 +0000

    Added Wallet as a valid output to getAPSigner

--- utils/signers/connectSignerFromPkey.ts
+++ utils/signers/connectSignerFromPkey.ts
@@ -1,11 +1,9 @@
-import {SignerWithAddress} from "@nomiclabs/hardhat-ethers/signers";
 import {Wallet} from "ethers";
 import {HardhatRuntimeEnvironment} from "hardhat/types";
 
 export async function connectSignerFromPkey(
   pkey: string,
   hre: HardhatRuntimeEnvironment
-): Promise<SignerWithAddress> {
-  const signer = new Wallet(pkey, hre.ethers.provider);
-  return hre.ethers.getSigner(signer.address);
+): Promise<Wallet> {
+  return new Wallet(pkey, hre.ethers.provider);
 }
