1a242e39f3f0a0025f603cff6b627eacfbabc20c
Nenad <n.misic@outlook.com>
2023-09-18 12:30:18 +0200

    Refactor GasFwdFactory

--- test/core/gasFwd/GasFwdFactory.ts
+++ test/core/gasFwd/GasFwdFactory.ts
@@ -16,82 +16,107 @@ use(smock.matchers);
 
 describe("GasFwdFactory", function () {
   const {ethers} = hre;
-  let owner: Signer;
+
+  let deployer: Signer;
   let admin: Signer;
   let user: Signer;
+
+  let gasFwdFactory: GasFwdFactory;
+
   let registrarFake: FakeContract<Registrar>;
 
-  async function deployGasFwdFactory(
-    owner: Signer,
-    admin: Signer,
-    registrar: string
-  ): Promise<GasFwdFactory> {
-    let GasFwd = new GasFwd__factory(admin);
-    let gasFwdImpl = await GasFwd.deploy();
-    await gasFwdImpl.deployed();
-    let GFF = new GasFwdFactory__factory(owner);
-    let gff = await GFF.deploy(gasFwdImpl.address, await admin.getAddress(), registrar);
-    await gff.deployed();
-    return gff;
-  }
-
   before(async function () {
-    const {deployer, apTeam1} = await getSigners(hre);
-    owner = deployer;
-    user = apTeam1;
+    const signers = await getSigners(hre);
+    deployer = signers.deployer;
+    user = signers.apTeam1;
 
     admin = await getProxyAdminOwner(hre);
-  });
 
-  beforeEach(async function () {
     registrarFake = await smock.fake<Registrar>(new Registrar__factory(), {
       address: genWallet().address,
     });
+
     const config = {
       ...DEFAULT_REGISTRAR_CONFIG,
       accountsContract: genWallet().address,
     };
     registrarFake.queryConfig.returns(config);
+
+    const GasFwd = new GasFwd__factory(deployer);
+    const gasFwd = await GasFwd.deploy();
+    gasFwdFactory = await deploy(
+      gasFwd.address,
+      await admin.getAddress(),
+      registrarFake.address,
+      deployer
+    );
   });
 
   describe("upon deployement", async function () {
-    it("reverts if the registrar address is invalid", async function () {
-      await expect(deployGasFwdFactory(owner, admin, ethers.constants.AddressZero)).to.be.reverted;
-    });
+    const gffInterface = GasFwdFactory__factory.createInterface();
 
-    it("successfully deploys", async function () {
-      expect(await deployGasFwdFactory(owner, admin, registrarFake.address));
+    it("reverts if the implementation address is invalid", async function () {
+      await expect(
+        deploy(
+          ethers.constants.AddressZero,
+          await admin.getAddress(),
+          registrarFake.address,
+          deployer
+        )
+      )
+        .to.be.revertedWithCustomError({interface: gffInterface}, "InvalidAddress")
+        .withArgs("_impl");
+    });
+    it("reverts if the admin address is invalid", async function () {
+      await expect(
+        deploy(genWallet().address, ethers.constants.AddressZero, registrarFake.address, deployer)
+      )
+        .to.be.revertedWithCustomError({interface: gffInterface}, "InvalidAddress")
+        .withArgs("_admin");
+    });
+    it("reverts if the registrar address is invalid", async function () {
+      await expect(
+        deploy(
+          genWallet().address,
+          await admin.getAddress(),
+          ethers.constants.AddressZero,
+          deployer
+        )
+      )
+        .to.be.revertedWithCustomError({interface: gffInterface}, "InvalidAddress")
+        .withArgs("_registrar");
     });
   });
 
   describe("upon create", async function () {
-    let gff: GasFwdFactory;
-    beforeEach(async function () {
-      gff = await deployGasFwdFactory(owner, admin, registrarFake.address);
-    });
-
     it("deploys a new proxy of the GasFwd contract", async function () {
-      expect(gff.address);
-      expect(await gff.create()).to.emit(gff, "GasFwdCreated");
+      await expect(gasFwdFactory.create()).to.emit(gasFwdFactory, "GasFwdCreated");
     });
   });
 
   describe("upon updateImplementation", async function () {
-    let gff: GasFwdFactory;
-    beforeEach(async function () {
-      gff = await deployGasFwdFactory(owner, admin, registrarFake.address);
-    });
-
     it("reverts if called by a non-owner ", async function () {
       await expect(
-        gff.connect(admin).updateImplementation(ethers.constants.AddressZero)
+        gasFwdFactory.connect(admin).updateImplementation(ethers.constants.AddressZero)
       ).to.be.revertedWith("Ownable: caller is not the owner");
     });
 
     it("reverts if the new impl address is invalid", async function () {
       await expect(
-        gff.updateImplementation(ethers.constants.AddressZero)
-      ).to.be.revertedWithCustomError(gff, "InvalidAddress");
+        gasFwdFactory.updateImplementation(ethers.constants.AddressZero)
+      ).to.be.revertedWithCustomError(gasFwdFactory, "InvalidAddress");
     });
   });
 });
+
+async function deploy(
+  impl: string,
+  admin: string,
+  registrar: string,
+  deployer: Signer
+): Promise<GasFwdFactory> {
+  let GFF = new GasFwdFactory__factory(deployer);
+  let gff = await GFF.deploy(impl, admin, registrar);
+  await gff.deployed();
+  return gff;
+}
