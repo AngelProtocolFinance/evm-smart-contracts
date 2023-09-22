87edfef60345281666f71b9c2ac88e1220d12dae
katzman <steve.r.katzman@gmail.com>
2023-09-22 03:47:03 +0000

    Some slight fixes to tasks for deploying GiftCards, config updates to support keeper address

--- tasks/deploy/deployGiftcard.ts
+++ tasks/deploy/deployGiftcard.ts
@@ -1,16 +1,16 @@
-import {deployGiftCard} from "contracts/accessory/gift-cards/scripts/deploy.ts";
+import { CONFIG } from "config";
+import {deployGiftCard} from "contracts/accessory/gift-cards/scripts/deploy";
 import {task} from "hardhat/config";
 import {getAddresses, isLocalNetwork, logger} from "utils";
 
 type TaskArgs = {
-  angelCoreStruct?: string;
   keeper: string;
   registrar?: string;
   skipVerify: boolean;
 };
 
 task("deploy:GiftCard", "Will deploy GiftCardContracts contract")
-  .addParam("keeper", "Keeper address for GiftCard contract.")
+  .addOptionalParam("keeper", "Keeper address for GiftCard contract, will lookup from config if not specified")
   .addOptionalParam(
     "registrar",
     "Registrar contract address. Will do a local lookup from contract-address.json if none is provided."
@@ -21,14 +21,15 @@ task("deploy:GiftCard", "Will deploy GiftCardContracts contract")
       const addresses = await getAddresses(hre);
 
       const registrar = taskArgs.registrar || addresses.registrar.proxy;
+      const keeper = taskArgs.keeper || CONFIG.PROD_CONFIG.GiftCardKeeper;
       const verify_contracts = !isLocalNetwork(hre) && !taskArgs.skipVerify;
 
       const GiftCardDataInput = {
-        keeper: taskArgs.keeper,
+        keeper: keeper,
         registrarContract: registrar,
       };
 
-      await deployGiftCard(GiftCardDataInput, verify_contracts, hre);
+      await deployGiftCard(GiftCardDataInput, addresses.multiSig.apTeam.proxy, verify_contracts, hre);
     } catch (error) {
       logger.out(error, logger.Level.Error);
     }
