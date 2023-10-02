1522ea08c096d41ae3bc3a44fc2e16fc0475a686
katzman <steve.r.katzman@gmail.com>
2023-10-02 22:41:27 +0000

    Refactored Flux to use abstract APStrategy_V1 instead of duplicating it and implementing the interface directly

--- contracts/integrations/flux/FluxStrategy.sol
+++ contracts/integrations/flux/FluxStrategy.sol
@@ -2,69 +2,18 @@
 // author: @stevieraykatz
 pragma solidity ^0.8.19;
 
-import {IStrategy} from "../../core/strategy/IStrategy.sol";
-import {Pausable} from "@openzeppelin/contracts/security/Pausable.sol";
+import {APStrategy_V1} from "../../core/strategy/APStrategy_V1.sol";
 import {IFlux} from "./IFlux.sol";
 import {FixedPointMathLib} from "../../lib/FixedPointMathLib.sol";
 import {ReentrancyGuard} from "@openzeppelin/contracts/security/ReentrancyGuard.sol";
+import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import {SafeERC20} from "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
 
-contract FluxStrategy is IStrategy, Pausable, ReentrancyGuard {
+contract FluxStrategy is APStrategy_V1, ReentrancyGuard {
   using FixedPointMathLib for uint256;
-
-  /*** CONSTNATS ***/
-  uint256 constant expScale = 1e18;
-
-  /*** STORAGE  ***/
-  StrategyConfig public config;
 
   constructor(StrategyConfig memory _config) {
     config = _config;
-  }
-
-  /*//////////////////////////////////////////////////////////////
-                                ADMIN
-  //////////////////////////////////////////////////////////////*/
-
-  modifier onlyAdmin() {
-    if (_msgSender() != config.admin) revert AdminOnly();
-    _;
-  }
-
-  modifier nonZeroAmount(uint256 amt) {
-    if (amt == 0) revert ZeroAmount();
-    _;
-  }
-
-  function pause() external onlyAdmin {
-    _pause();
-  }
-
-  function unpause() external onlyAdmin {
-    _unpause();
-  }
-
-  /// @notice Check whether the contract is paused
-  /// @dev Make public the state of the Pausable contract's `paused` state
-  /// @return paused the current state of the paused boolean
-  function paused() public view override(IStrategy, Pausable) returns (bool) {
-    return super.paused();
-  }
-
-  /*//////////////////////////////////////////////////////////////
-                                CONFIG
-  //////////////////////////////////////////////////////////////*/
-  /// @notice Returns the config struct
-  /// @return Config the current strategy config
-  function getStrategyConfig() external view returns (StrategyConfig memory) {
-    return config;
-  }
-
-  /// @notice Set the strategy config
-  /// @dev This method must be access controlled. The config overwrites the stored config in its entirety
-  /// @param _newConfig The StrategyConfig that willbe stored and referenced within the strategy contract
-  function setStrategyConfig(StrategyConfig memory _newConfig) external onlyAdmin {
-    config = _newConfig;
-    emit ConfigChanged(config);
   }
 
   /*//////////////////////////////////////////////////////////////
@@ -79,7 +28,7 @@ contract FluxStrategy is IStrategy, Pausable, ReentrancyGuard {
   /// @return yieldTokenAmt the qty of `config.yieldToken` that were yielded from the deposit action
   function deposit(
     uint256 amt
-  ) external payable whenNotPaused nonReentrant nonZeroAmount(amt) returns (uint256) {
+  ) external override payable whenNotPaused nonReentrant nonZeroAmount(amt) returns (uint256) {
     if (!IFlux(config.baseToken).transferFrom(_msgSender(), address(this), amt)) {
       revert TransferFailed();
     }
@@ -103,7 +52,7 @@ contract FluxStrategy is IStrategy, Pausable, ReentrancyGuard {
   /// @return baseTokenAmt the qty of `config.baseToken` that are approved for transfer by msg.sender
   function withdraw(
     uint256 amt
-  ) external payable whenNotPaused nonReentrant nonZeroAmount(amt) returns (uint256) {
+  ) external override payable whenNotPaused nonReentrant nonZeroAmount(amt) returns (uint256) {
     if (!IFlux(config.yieldToken).transferFrom(_msgSender(), address(this), amt)) {
       revert TransferFailed();
     }
@@ -122,22 +71,22 @@ contract FluxStrategy is IStrategy, Pausable, ReentrancyGuard {
   /// @dev This method expects that the `amt` provided is denominated in `baseToken`
   /// @param amt the qty of the `baseToken` that should be checked for conversion rates
   /// @return yieldTokenAmt the expected qty of `yieldToken` if this strategy received `amt` of `baseToken`
-  function previewDeposit(uint256 amt) external view returns (uint256) {
-    // Exchange Rate == (expScale * USDC) / fUSDC
+  function previewDeposit(uint256 amt) external override view returns (uint256) {
+    // Exchange Rate == (EXP_SCALE * USDC) / fUSDC
     uint256 exRate = IFlux(config.yieldToken).exchangeRateStored();
-    // Expected fUSDC == (amtUSDC * expScale / exRate)
-    return amt.mulDivDown(expScale, exRate);
+    // Expected fUSDC == (amtUSDC * EXP_SCALE / exRate)
+    return amt.mulDivDown(EXP_SCALE, exRate);
   }
 
   /// @notice Provide an estimate for the current exchange rate for a given withdrawal
   /// @dev This method expects that the `amt` provided is denominated in `yieldToken`
   /// @param amt the qty of the `yieldToken` that should be checked for conversion rates
   /// @return yieldTokenAmt the expected qty of `baseToken` if this strategy received `amt` of `yieldToken`
-  function previewWithdraw(uint256 amt) external view returns (uint256) {
-    // Exchange Rate == (expScale * USDC) / fUSDC
+  function previewWithdraw(uint256 amt) external override view returns (uint256) {
+    // Exchange Rate == (EXP_SCALE * USDC) / fUSDC
     uint256 exRate = IFlux(config.yieldToken).exchangeRateStored();
-    // Expected USDC == (amtfUSDC * exRate) / expScale
-    return amt.mulDivDown(exRate, expScale);
+    // Expected USDC == (amtfUSDC * exRate) / EXP_SCALE
+    return amt.mulDivDown(exRate, EXP_SCALE);
   }
 
   /*//////////////////////////////////////////////////////////////
